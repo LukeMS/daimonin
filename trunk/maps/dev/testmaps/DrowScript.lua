38288169d3f0bb0de4c2d702e2d90db6703ff894
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2005-02-02 19:14:08 +0000

    Added more exhaustive tests for recursion and globals, but couldn't trigger the suspected bug.

--- trunk/maps/dev/testmaps/DrowScript.lua
+++ trunk/maps/dev/testmaps/DrowScript.lua
@@ -32,16 +32,45 @@ function topicRecursive()
 	end
 end
 
+function topicRecursive2()
+	-- Tests whether the "event" global is untouched after
+	-- a multi-level recursive script trigger.
+	-- Also tests external classes (topic list) for wierd globals changes
+
+	local message = event.message
+	event.me:Say("Ok, initiating recursion... (message was "..message..")");
+	event.me:Communicate("recursive_part_2")
+
+	local newmessage = event.message
+	if newmessage == message then
+    	event.me:Say("Test successful, the global event.message didn't change from " .. message);
+	else
+    	event.me:Say("Test failed! Event.message changed from "..message.." to "..newmessage);
+	end
+end
+
+function topicRecursive3()
+	event.me:Say("Third level of recursion. Wee! (message = "..event.message..")");
+	local tl = TopicList()
+	tl:setDefault( function() event.me:Say("lvl3 tl: something wierd happened to the topiclist class") end)
+	tl:addTopics("recursive", function() event.me:Say("lvl3 tl: recursive ( WRONG )") end)
+	tl:addTopics("recursive2", function() event.me:Say("lvl3 tl: recursive2 ( WRONG )") end)
+	tl:addTopics("recursive3", function() event.me:Say("lvl3 tl: recursive3 ( CORRECT )") end)
+	tl:checkMessage()
+end
+
 tl = TopicList()
 tl:setDefault([[
 
 Welcome, you can test
-^recursive^ scripts or the storage of ^data^.
+^recursive^, ^recursive2^ scripts or the storage of ^data^.
 If you want to test the second say
 data <message>
 This message will be the new message of the lost soul.]])
 
 tl:addTopics("recursive", topicRecursive)
+tl:addTopics("recursive2", topicRecursive2)
+tl:addTopics("recursive3", topicRecursive3)
 tl:addTopics("data.*", topicData)
 
 tl:checkMessage()
