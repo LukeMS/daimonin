57c07aaf21d712b4e31b9c63aa00e79f91fe8372
thober <thober@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2005-02-03 22:58:30 +0000

    Added a kind of PlayerInfo functionality to the DataTable

--- trunk/maps/dev/testmaps/DrowScript.lua
+++ trunk/maps/dev/testmaps/DrowScript.lua
@@ -17,10 +17,30 @@ function topicData()
 	end
 end
 
+function topicData2()
+	-- Tests the storage of data for a player
+
+	local msg = string.split(event.message)
+	if table.getn(msg) < 3 then
+		event.me:SayTo(event.activator, "Say '^data2^ <player> <message>'")
+		return
+	end
+	local dt = DataTable("data_store_test", msg[2])
+	msg = table.concat(msg, " ", 3)
+	dt:set("activator", event.activator)
+	dt:set("message", msg)
+	local msg2 = dt:get("message")
+	if msg2 == msg then
+		event.me:SayTo(event.activator, "Message '" .. msg .. "' saved.\nThe player will see this message if (s)he talks to the lost soul.")
+	else
+		event.me:SayTo(event.activator, "Error: Message wasn't saved!")
+	end
+end
+
 function topicInfo()
-	local msg = "\nContents of data_store:"
+	local msg = "\nContents of data_store:\nGlobal:"
 	for k1, v1 in pairs(data_store) do
-		if k1 ~= "n" then
+		if k1 ~= "n" and k1 ~= "_players" then
 			msg = msg .. "\n'" .. tostring(k1) .. "' => DataTable:\nLast change: " .. os.date("!%Y-%m-%d %H:%M:%S %Z", v1._changed) .. "\nContents:\n---"
 			for k2, v2 in pairs(v1) do
 				if k2 ~= "_changed" then
@@ -28,6 +48,21 @@ function topicInfo()
 				end
 			end
 			msg = msg .. "\n---"
+		end
+	end
+	msg = msg .. "\nLocal:"
+	for k1, v1 in pairs(data_store._players) do
+		msg = msg .. "\nPlayer '" .. tostring(k1) .. "':"
+		for k2, v2 in pairs(v1) do
+			if k2 ~= "n" then
+				msg = msg .. "\n'" .. tostring(k2) .. "' => DataTable:\nLast change: " .. os.date("!%Y-%m-%d %H:%M:%S %Z", v2._changed) .. "\nContents:\n---"
+				for k3, v3 in pairs(v2) do
+					if k3 ~= "_changed" then
+						msg = msg .. "\n'" .. tostring(k3) .. "' => '" .. tostring(v3) .. "'"
+					end
+				end
+				msg = msg .. "\n---"
+			end
 		end
 	end
 	event.me:SayTo(event.activator, msg)
@@ -89,15 +124,18 @@ Available tests/topics:
 ^recursive^
 ^recursive2^
 ^data^ <message>
+^data2^ <player> <message>
 ^info^
 
 ^recursive^ and ^recursive2^ tests recursive scripts.
 ^data^ tests the storage of global data.
+^data2^ tests the storage of data for a player.
 ^info^ dumps the contents of 'data_store'.]])
 
 tl:addTopics("recursive", topicRecursive)
 tl:addTopics("recursive2", topicRecursive2)
 tl:addTopics("recursive3", topicRecursive3)
+tl:addTopics("data2.*", topicData2)
 tl:addTopics("data.*", topicData)
 tl:addTopics("info", topicInfo)
 
--- trunk/maps/dev/testmaps/lost_soul.lua
+++ trunk/maps/dev/testmaps/lost_soul.lua
@@ -1,14 +1,31 @@
-dt = DataTable("data_store_test")
+local b_msg = false
 
+dt = DataTable("data_store_test")
 msg = dt:get("message")
 
 if msg then
 	ac = dt:get("activator")
+	b_msg = true
 	if ac ~= nil then
-		event.me:SayTo(event.activator, "\nA message from " .. ac.name .. ":\n" .. msg)
+		event.me:SayTo(event.activator, "\nA global message from " .. ac.name .. ":\n" .. msg)
 	else
-		event.me:SayTo(event.activator, "\nA message from an unkown object:\n" .. msg)
+		event.me:SayTo(event.activator, "\nA global message from an unkown object:\n" .. msg)
 	end
-else
+end
+
+dt = DataTable("data_store_test", event.activator)
+msg = dt:get("message")
+
+if msg then
+	ac = dt:get("activator")
+	b_msg = true
+	if ac ~= nil then
+		event.me:SayTo(event.activator, "\nA local message from " .. ac.name .. " for you:\n" .. msg)
+	else
+		event.me:SayTo(event.activator, "\nA local message from an unkown object for you:\n" .. msg)
+	end
+end
+
+if not b_msg then
 	event.activator:Write("The lost soul remains quiet.", game.COLOR_RED)
 end
