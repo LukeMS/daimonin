37bf518614077cf5a367770e3890596a959a7b42
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2005-04-08 14:40:35 +0000

    New testmap for the AI system

---
+++ maps/dev/testmaps/ai_ogrescript.lua
@@ -0,0 +1,25 @@
+--[[ 
+--  Shows how the AI object can be used to modify a mob's 
+--  friendship value in a normal chat script 
+--]]
+require("topic_list");
+
+function modify_friendship(op, friend, change)
+    local ai = op:GetAI()
+    ai:Register(friend, change, 0)
+    op:Say("My new friendship towards " .. friend.name .. ": " .. ai:GetFriendship(friend))
+end
+
+function insult_func()
+    modify_friendship(event.me, event.activator, -200)
+end
+
+function compliment_func()
+    modify_friendship(event.me, event.activator, 200)
+end
+
+tl = TopicList()
+tl:AddTopics("insult", insult_func)
+tl:AddTopics("compliment", compliment_func)
+tl:SetDefault("You can ^insult^ or ^compliment^ me to change my attitude towards you")
+tl:CheckMessage(event)
