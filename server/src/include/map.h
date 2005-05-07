2c1bc804f555b440e2dbf445aa2c79283501a20d
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2005-05-07 18:27:54 +0000

    Added environment sensors

--- server/src/include/map.h
+++ server/src/include/map.h
@@ -116,6 +116,7 @@
 #define SET_MAP_FLAGS(M,X,Y,C)  ( (M)->spaces[(X) + (M)->width * (Y)].flags = C )
 #define GET_MAP_LIGHT(M,X,Y)    ( (M)->spaces[(X) + (M)->width * (Y)].light )
 #define SET_MAP_LIGHT(M,X,Y,L)  ( (M)->spaces[(X) + (M)->width * (Y)].light = (sint8) L )
+#define GET_MAP_LIGHT_VALUE(M,X,Y)    ( (M)->spaces[(X) + (M)->width * (Y)].light_value )
 
 #define GET_MAP_OB(M,X,Y)   ( (M)->spaces[(X) + (M)->width * (Y)].first )
 #define GET_MAP_OB_LAST(M,X,Y)  ( (M)->spaces[(X) + (M)->width * (Y)].last )
