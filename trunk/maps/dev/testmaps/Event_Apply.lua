b0db125911423c190170106fdbbdf049350be38d
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2005-05-15 14:49:17 +0000

    Tests for talking items in inventory

--- trunk/maps/dev/testmaps/Event_Apply.lua
+++ trunk/maps/dev/testmaps/Event_Apply.lua
@@ -1,4 +1,9 @@
-event.activator:Write("This is an APPLY script!")
+if event.me.f_applied then
+    event.activator:Write("This is an UNAPPLY script!")
+else
+    event.activator:Write("This is an APPLY script!")
+end
+    
 if(event.options == 'override') then
     event.activator:Write("Overriding normal behaviour...")
     event.returnvalue = 1
