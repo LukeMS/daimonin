27286d2a74dace3fd5284c52103c472614a5657d
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2005-05-21 13:51:10 +0000

    Lua security fixes:  - possibly dangerous IO functions have either been removed or    wrapped with input verification functions

--- trunk/maps/lua/plugin_init.lua
+++ trunk/maps/lua/plugin_init.lua
@@ -8,7 +8,12 @@
 -- Also note that this script is _only_ loaded at server startup
 ]]
 
+require("security")
 require("data_store")
+
+--
+-- A couple of extensions to the string library
+--
 
 function string.capitalize(s, b_keep)
 	local f = string.sub(s, 1, 1)
@@ -50,7 +55,7 @@ function _error(msg)
         return false
     end
 
-    if event then
+    if event and game:IsValid(event) then
         msg_wiz_obj(event.activator)
         msg_wiz_obj(event.me)
         msg_wiz_obj(event.other)
@@ -59,8 +64,10 @@ function _error(msg)
     return msg
 end
 
+-- Shutdown hook called when server unloads the plugin
 function _shutdown()
 	_data_store.save(true)
 end
 
+-- Finished with the initialization
 print "    plugin_init.lua loaded successfully"
