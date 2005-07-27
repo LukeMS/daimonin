6ce3b9dfcd7bf0927d4d081924a5df94394064f0
polyveg <polyveg@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2005-07-27 20:53:36 +0000

    Update to Ogre 1.0.3 - Step 1

---
+++ client3d/make/win32/CodeBlocks/Setup.bat
@@ -0,0 +1,39 @@
+rem ====================================
+rem (c) 2005 by the Daimonin team.
+rem     www.daimonin.net
+rem ====================================
+
+rem ====================================
+rem Unpack all files.
+rem ====================================
+..\..\_Tools_\gunzip ogre_lib.tgz 
+..\..\_Tools_\tar xvf ogre_lib.tar
+..\..\_Tools_\gunzip ogre_inc.tgz
+..\..\_Tools_\tar xvf ogre_inc.tar
+..\..\_Tools_\gunzip ogre_dll.tgz
+..\..\_Tools_\tar xvf ogre_dll.tar
+
+..\..\_Tools_\gunzip fmod_lib.tgz 
+..\..\_Tools_\tar xvf fmod_lib.tar
+..\..\_Tools_\gunzip fmod_inc.tgz
+..\..\_Tools_\tar xvf fmod_inc.tar
+..\..\_Tools_\gunzip fmod_dll.tgz
+..\..\_Tools_\tar xvf fmod_dll.tar
+
+rem ====================================
+rem Copy the dll's to main folder.
+rem ====================================
+move ogre_dll\*.* ..\..\..\
+move fmod_dll\*.* ..\..\..\
+rmdir ogre_dll
+rmdir fmod_dll
+
+rem ====================================
+rem We need the Code::Blocks plugins.
+rem ====================================
+copy plugins.cfg ..\..\..\
+
+rem ====================================
+rem Clean up.
+rem ====================================
+del *.tar
