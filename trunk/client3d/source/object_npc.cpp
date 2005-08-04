6a501fddabe481b92b77ce1c08b3ac9b757cc70a
polyveg <polyveg@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2005-08-04 16:29:05 +0000

    updated linux specific stuff

--- trunk/client3d/source/object_npc.cpp
+++ trunk/client3d/source/object_npc.cpp
@@ -27,7 +27,7 @@ http://www.gnu.org/licenses/licenses.html
 #include "textwindow.h"
 #include "spell_manager.h"
 #include "event.h"
-#include "tileManager.h"
+#include "TileManager.h"
 
 extern Camera *mCamera;
 
