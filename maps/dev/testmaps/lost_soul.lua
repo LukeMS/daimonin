d255e48cb30f07a5c8b689d192cd3ce1cad9e72b
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2006-09-02 08:05:28 +0000

    Re-indented

--- maps/dev/testmaps/lost_soul.lua
+++ maps/dev/testmaps/lost_soul.lua
@@ -4,36 +4,36 @@ ds = DataStore("data_store_test")
 msg = ds:Get("message")
 
 if msg then
-ac = ds:Get("activator")
-b_msg = true
-if ac ~= nil then
-event.me:SayTo(event.activator, "\nA global message from " .. ac.name .. ":\n" .. msg)
-else
-event.me:SayTo(event.activator, "\nA global message from an unkown object:\n" .. msg)
-end
+    ac = ds:Get("activator")
+    b_msg = true
+    if ac ~= nil then
+        event.me:SayTo(event.activator, "\nA global message from " .. ac.name .. ":\n" .. msg)
+    else
+        event.me:SayTo(event.activator, "\nA global message from an unkown object:\n" .. msg)
+    end
 end
 
 func = ds:Get("function")
 if func and type(func) == 'function' then
-b_msg = true
-event.me:SayTo(event.activator, "A function was stored. Calling it...")
-func(event)
-event.me:SayTo(event.activator, "Done calling the function...")
+    b_msg = true
+    event.me:SayTo(event.activator, "A function was stored. Calling it...")
+    func(event)
+    event.me:SayTo(event.activator, "Done calling the function...")
 end
 
 ds = DataStore("data_store_test", event.activator)
 msg = ds:Get("message")
 
 if msg then
-ac = ds:Get("activator")
-b_msg = true
-if ac ~= nil then
-event.me:SayTo(event.activator, "\nA local message from " .. ac.name .. " for you:\n" .. msg)
-else
-event.me:SayTo(event.activator, "\nA local message from an unkown object for you:\n" .. msg)
-end
+    ac = ds:Get("activator")
+    b_msg = true
+    if ac ~= nil then
+        event.me:SayTo(event.activator, "\nA local message from " .. ac.name .. " for you:\n" .. msg)
+    else
+        event.me:SayTo(event.activator, "\nA local message from an unkown object for you:\n" .. msg)
+    end
 end
 
 if not b_msg then
-event.activator:Write("The lost soul remains quiet.", game.COLOR_RED)
+    event.activator:Write("The lost soul remains quiet.", game.COLOR_RED)
 end
--- maps/dev/testmaps/skills.lua
+++ maps/dev/testmaps/skills.lua
@@ -6,18 +6,18 @@ whoami = event.me
 tl = TopicList()
 
 tl:AddTopics("learn (.*)",
-function(skillname)
-skill = game:GetSkillNr(skillname)
-if skill == -1 then
-whoami:SayTo(activator,"Unknown skill \""..skillname.."\"" )
-else
-if activator:FindSkill(skill) ~= nil then
-whoami:SayTo(activator,"You already learned this skill." )
-else
-activator:AcquireSkill(skill, game.LEARN)
-end
-end
-end
+    function(skillname)
+        skill = game:GetSkillNr(skillname)
+        if skill == -1 then
+            whoami:SayTo(activator,"Unknown skill \""..skillname.."\"" )
+        else
+            if activator:FindSkill(skill) ~= nil then
+                whoami:SayTo(activator,"You already learned this skill." )
+            else
+                activator:AcquireSkill(skill, game.LEARN)
+            end
+        end
+    end
 )
 
 tl:SetDefault("\nI am the Skillgiver.\nSay ^learn <skillname>^ or ^unlearn <skillname>^ (Note: unlearn skills is not implemented atm - the ideas of the daimonin skill system is to collect the skills - and don't lose them).")
