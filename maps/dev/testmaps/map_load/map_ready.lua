caaa41982208d1d7f2c729241b75fe5fcad3c885
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2006-11-21 19:58:54 +0000

    Fixes in tweaked scripts

--- maps/dev/testmaps/map_load/map_ready.lua
+++ maps/dev/testmaps/map_load/map_ready.lua
@@ -11,24 +11,24 @@ local ib = InterfaceBuilder()
 ib:SetHeader(me, me.name)
 
 local function topicDefault()
-	ib:AddMsg("We test here to init, load & jump to an instance by script.")
+    ib:AddMsg("We test here to init, load & jump to an instance by script.")
 
-	if pl:CheckInstance(instance_path) then
-		ib:AddMsg("\n\nThis instance is ACTIVE for you!\n")
-		ib:AddLink("Neutralize (delete) your instance", "remInstance")			
-	end
-    	ib:AddLink("Load & jump to instance", "jmpinstance")
-	pl:Interface(1, ib:Build())
+    if pl:CheckInstance(instance_path) then
+        ib:AddMsg("\n\nThis instance is ACTIVE for you!\n")
+        ib:AddLink("Neutralize (delete) your instance", "remInstance")			
+    end
+    ib:AddLink("Load & jump to instance", "jmpinstance")
+    pl:Interface(1, ib:Build())
 end
 
 local function jmpInstance()
-	pl:SetPosition(pl:StartNewInstance(instance_path), 1, 1)
-	pl:Interface(-1, "")
+    pl:SetPosition(pl:StartNewInstance(instance_path), 1, 1)
+    pl:Interface(-1, "")
 end
 
 local function remInstance()
-	pl:DeleteInstance(instance_path)
-	topicDefault()
+    pl:DeleteInstance(instance_path)
+    topicDefault()
 end
 
 tl = TopicList()
