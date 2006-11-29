b68368c1b37563e535ed33c82f8495d57ef62953
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2006-11-29 19:50:51 +0000

    Port unit tests to new map model

--- trunk/server/src/tests/test_object.c
+++ trunk/server/src/tests/test_object.c
@@ -127,7 +127,7 @@ START_TEST (object_type_beacon)
     fail_if(locate_beacon(b1_name) != NULL, "Inv beacon available before test");
     fail_if(locate_beacon(b2_name) != NULL, "Map beacon available before test");
     
-    map = ready_map_name(path, 0, NULL);
+    map = ready_map_name(NULL, path, MAP_STATUS_MULTI, NULL);
     fail_unless(map != NULL, "Couldn't load %s", path);
     
     /* Locate the two beacons by hardcoded coordinates */
@@ -151,7 +151,7 @@ END_TEST
 
 START_TEST (object_type_check_inv)
 {
-    mapstruct *map = ready_map_name(add_string("/dev/unit_tests/test_check_inv"), 0, NULL);
+    mapstruct *map = ready_map_name(NULL, add_string("/dev/unit_tests/test_check_inv"), MAP_STATUS_MULTI, NULL);
 
     object *check1 = locate_beacon(find_string("check1"))->env;
     object *check2 = locate_beacon(find_string("check2"))->env;
