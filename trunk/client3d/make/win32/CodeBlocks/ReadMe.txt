6992e31582c1138133ef3d6d79622904575d564b
polyveg <polyveg@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-01-04 20:02:32 +0000

    win related build stuff will be hold extern.

--- trunk/client3d/make/win32/CodeBlocks/ReadMe.txt
+++ trunk/client3d/make/win32/CodeBlocks/ReadMe.txt
@@ -6,6 +6,7 @@ Install the latest nightly build of Code::Blocks.
 Install MinGW C++ Toolbox (Read the wiki pages on Ogre3d).
 
 ----
+0) Copy the client3d_build_cb.tgz (from the sourceforge download page) into client3d\make\win32\VC80\
 1) Start the Setup.bat
 2) Dblclk on the Code::Blocks project file
 3) Select a build target (Release or Debug)
--- trunk/client3d/make/win32/VC80/Setup.bat
+++ trunk/client3d/make/win32/VC80/Setup.bat
@@ -6,18 +6,18 @@ rem ====================================
 rem ====================================
 rem Unpack all files.
 rem ====================================
+..\..\_Tools_\gunzip -c client3d_build_vc.tgz >build.tar
+..\..\_Tools_\tar xvf build.tar
 ..\..\_Tools_\gunzip -c ogre_lib.tgz >ogre_lib.tar
 ..\..\_Tools_\tar xvf ogre_lib.tar
 ..\..\_Tools_\gunzip -c ogre_inc.tgz >ogre_inc.tar
 ..\..\_Tools_\tar xvf ogre_inc.tar
 ..\..\_Tools_\gunzip -c ogre_dll.tgz >ogre_dll.tar
 ..\..\_Tools_\tar xvf ogre_dll.tar
-
 ..\..\_Tools_\gunzip -c fmod_lib.tgz >fmod_lib.tar
 ..\..\_Tools_\tar xvf fmod_lib.tar
 ..\..\_Tools_\gunzip -c fmod_dll.tgz >fmod_dll.tar
 ..\..\_Tools_\tar xvf fmod_dll.tar
-
 ..\..\_Tools_\gunzip -c sdl_lib.tgz >sdl_lib.tar
 ..\..\_Tools_\tar xvf sdl_lib.tar
 ..\..\_Tools_\gunzip -c sdl_dll.tgz >sdl_dll.tar
@@ -41,3 +41,10 @@ rem ====================================
 rem Clean up.
 rem ====================================
 del *.tar
+del fmod_dll.tgz
+del fmod_lib.tgz
+del ogre_dll.tgz
+del ogre_lib.tgz
+del ogre_inc.tgz
+del sdl_lib.tgz
+del sdl_dll.tgz
