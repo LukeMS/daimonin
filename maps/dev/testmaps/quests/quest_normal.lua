cd3fb84a6d9d75667ce6eb1a174c826b5e95a61a
tehill <tehill@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-01-21 14:50:15 +0000

    added skill_group option and test for game.QSTAT_DISALLOW

--- maps/dev/testmaps/quests/quest_normal.lua
+++ maps/dev/testmaps/quests/quest_normal.lua
@@ -7,36 +7,39 @@ local pl        = event.activator
 local me        = event.me
 local msg       = string.lower(event.message)
 -- quest names must be unique, the player will store the name forever
--- level and skill are optional
+-- level and skill_group are optional
 local level = 1
--- skill must be one of the 6 game constants or use game.ITEM_SKILL_NO for no skill
-local skill = game.ITEM_SKILL_PHYSICAL
-local q_mgr_1 = QuestManager(pl, "Dev Normal Test Quest", level, skill)
-
-
+-- skill_group must be one of the 6 game item skill constants  or use game.ITEM_SKILL_NO for main level
+local skill_group = game.ITEM_SKILL_PHYSICAL
+local q_mgr_1 = QuestManager(pl, "Dev Normal Test Quest", level, skill_group)
 local ib = InterfaceBuilder()
 ib:SetHeader(me, me.name)
 
 local function topicDefault()
-    if q_mgr_1:GetStatus() < game.QSTAT_DONE then
-        ib:AddMsg("[DEVMSG] The quest status is: ".. q_mgr_1:GetStatus() .."\n\n")
-        if q_mgr_1:GetStatus() == game.QSTAT_NO then
-            ib:SetTitle("Normal Test Quest")
-            ib:AddMsg("[INTRO] A 'normal' quest just increase a counter. There is no item involved. There is a QUEST_TRIGGER inside an object (here a chest) which inc the counter.")
-            ib:AddLink("Start Normal Test Quest", "startq1")
-        else
-            ib:SetTitle("Normal Test Quest solved?")
-            ib:AddMsg("[pending] You have done the quest?")
-            ib:AddLink("Finish Normal Test Quest", "checkq1")
-        end
-    else
-        pl:Write(me.name .." has nothing to say.", game.COLOR_NAVY)
+    if q_mgr_1:GetStatus() == game.QSTAT_DISALLOW then
+        pl:Write(me.name .." tells you to come back when your weapon skills are better", game.COLOR_NAVY)
         pl:Interface(-1, "") 
         return
+    else
+        if q_mgr_1:GetStatus() < game.QSTAT_DONE then
+            ib:AddMsg("[DEVMSG] The quest status is: ".. q_mgr_1:GetStatus() .."\n\n")
+            if q_mgr_1:GetStatus() == game.QSTAT_NO then
+                ib:SetTitle("Normal Test Quest")
+                ib:AddMsg("[INTRO] A 'normal' quest just increase a counter. There is no item involved. There is a QUEST_TRIGGER inside an object (here a chest) which inc the counter.")
+                ib:AddLink("Start Normal Test Quest", "startq1")
+            else
+                ib:SetTitle("Normal Test Quest solved?")
+                ib:AddMsg("[pending] You have done the quest?")
+                ib:AddLink("Finish Normal Test Quest", "checkq1")
+            end
+        else
+            pl:Write(me.name .." has nothing to say.", game.COLOR_NAVY)
+            pl:Interface(-1, "") 
+            return
+        end
+        pl:Interface(1, ib:Build())
     end
-    pl:Interface(1, ib:Build())
 end
-
 -- quest body (added to player quest obj for quest list)
 local function quest_icons1()
     ib:AddIcon("quest-reward-name", "shield.101", "i am the stats/description line") 
@@ -120,9 +123,9 @@ tl = TopicList()
 tl:AddGreeting(nil, topicDefault)
 tl:SetDefault(topicDefault)
 if q_mgr_1:GetStatus() < game.QSTAT_DONE then
-    tl:AddTopics("startq1", topStartQ1) 
-    tl:AddTopics("acceptq1", topAcceptQ1) 
-    tl:AddTopics("checkq1", topCheckQ1) 
-    tl:AddTopics("finishq1", topFinishQ1) 
+    tl:AddTopics("startq1", topStartQ1)
+    tl:AddTopics("acceptq1", topAcceptQ1)
+    tl:AddTopics("checkq1", topCheckQ1)
+    tl:AddTopics("finishq1", topFinishQ1)
 end
 tl:CheckMessage(event)
