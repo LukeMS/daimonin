f9d4969155e1542446cb7f59bf7b5495dcb670ce
christianhujer <christianhujer@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-01-22 22:15:00 +0000

    Added script for fixing svn:mime-type, svn:eol-style and svn:ignore properties.

---
+++ trunk/fixMimeAndIgnore.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# Shell script for fixing the properties of files in arch/ and maps/.
+
+mapsIgnore=".dedit"
+archIgnore="Thumbs.db"
+echo=
+
+find arch -name "*.png" -print0 | xargs -0 $echo svn propset svn:mime-type image/png
+find arch -name "*.arc" -print0 | xargs -0 $echo svn propset svn:mime-type text/plain
+find arch -name "*.arc" -print0 | xargs -0 $echo svn propset svn:eol-style LF
+find arch -name "*.anim" -print0 | xargs -0 $echo svn propset svn:mime-type text/plain
+find arch -name "*.anim" -print0 | xargs -0 $echo svn propset svn:eol-style LF
+find arch -type d -not -path "*/.svn/*" -not -name ".svn" -print0 | xargs -0 $echo svn propset svn:ignore "$mapsIgnore"
+
+find maps -type f -not -path "*/.svn/*" -print0 | xargs -0 $echo svn propset svn:mime-type text/plain
+find maps -type f -not -path "*/.svn/*" -print0 | xargs -0 $echo svn propset svn:eol-style LF
+find maps -type d -not -path "*/.svn/*" -not -name ".svn" -print0 | xargs -0 $echo svn propset svn:ignore "$mapsIgnore"
+
+svn commit -m "Fixed svn:eol-style, svn:ignore and svn:mime-type properties."
