b42a624d8a8574633f23a0d17f8f3d10102df874
christianhujer <christianhujer@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-01-22 22:51:01 +0000

    Fixed svn:eol-style, svn:ignore and svn:mime-type properties.

--- fixMimeAndIgnore.sh
+++ fixMimeAndIgnore.sh
@@ -16,4 +16,7 @@ find maps -type f -not -path "*/.svn/*" -print0 | xargs -0 $echo svn propset svn
 find maps -type f -not -path "*/.svn/*" -print0 | xargs -0 $echo svn propset svn:eol-style LF
 find maps -type d -not -path "*/.svn/*" -not -name ".svn" -print0 | xargs -0 $echo svn propset svn:ignore "$mapsIgnore"
 
+find maps arch -name "*.txt" -print0 | xargs -0 $echo svn propset svn:mime-type text/plain
+find maps arch -name "*.txt" -print0 | xargs -0 $echo svn propset svn:eol-style native
+
 svn commit -m "Fixed svn:eol-style, svn:ignore and svn:mime-type properties."
