272c01617d48dc513e985ac90461c4c47107c478
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-02-01 22:23:20 +0000

    Unit test for negative case of teleport outside of map

--- trunk/server/src/tests/test_teleporter.c
+++ trunk/server/src/tests/test_teleporter.c
@@ -60,6 +60,30 @@ START_TEST (simple_teleporter_without_path)
 }
 END_TEST
 
+START_TEST (bad_teleporter_without_path)
+{
+    shstr *path = add_string("/dev/unit_tests/test_teleporter");
+    mapstruct *map = ready_map_name(path, path, MAP_STATUS_MULTI, NULL);
+
+    object *lever = locate_beacon(find_string("lever_1"))->env;
+    object *apple = locate_beacon(find_string("apple_1"))->env;
+    object *teleport = apple->below ? apple->below : apple->above;
+ 
+    fail_unless(apple->x == 1 && apple->y == 1, "Apple initially in incorrect spot");
+    fail_unless(apple->map == map, "Apple initially in incorrect map");
+
+    /* Set telportation location to point outside map */
+    teleport->stats.hp = 100;
+    teleport->stats.sp = 100;
+
+    /* Switch the lever and make sure the inverse is true */
+    manual_apply(lever, lever, 0);
+    
+    fail_unless(apple->x == 1 && apple->y == 1 && apple->map == map, "Apple was teleported with bad teleporter");
+    fail_unless(QUERY_FLAG(teleport, FLAG_REMOVED), "Bad teleporter wasn't removed");
+}
+END_TEST
+
 Suite *teleporter_suite(void)
 {
   Suite *s = suite_create("Teleporter");
@@ -69,6 +93,7 @@ Suite *teleporter_suite(void)
   
   suite_add_tcase (s, tc_core);
   tcase_add_test(tc_core, simple_teleporter_without_path);
+  tcase_add_test(tc_core, bad_teleporter_without_path);
 
   return s;
 }
