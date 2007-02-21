c1398b90d1e8af9c0bc45698c11eeb0a6231699d
jeffg1 <jeffg1@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-02-21 22:11:25 +0000

    Add MH and CLW to priest script and fix a couple of existing bugs.

--- maps/scripts/church.lua
+++ maps/scripts/church.lua
@@ -8,9 +8,16 @@ local msg = string.lower(event.message)
 local ib = InterfaceBuilder()
 ib:SetHeader(me, me.name)
 
+local sum
+local spell
+
 local function topicDefault()
     ib:SetTitle("The Church of the Tabernacle")
-    ib:SetMsg("Hello! I am " .. me.name ..".\n\nWelcome to the church of the Tabernacle!\n\nIf you are confused by my services, i can ^explain^ it.\nYou need some of our services?")
+    ib:SetMsg("Hello! I am " .. me.name ..".\n\nWelcome to the church of the Tabernacle!\n\n")
+    ib:AddMsg("If you are confused by my services, I can ^explain^ it.\nYou need some of our services?")
+    ib:AddLink("Teach Minor Healing", "teach healing")
+    ib:AddLink("Teach Cause Light Wounds", "teach wounds")
+    ib:AddLink("", "")
     ib:AddLink("Remove Death Sickness", "cast sick")
     ib:AddLink("Remove Depletion", "cast deplete")
     ib:AddLink("Restoration", "cast restore")
@@ -18,13 +25,16 @@ local function topicDefault()
     ib:AddLink("Cure Poison", "cast poison")
     ib:AddLink("Remove Curse from items", "cast curse")
     ib:AddLink("Remove Damnation from items", "cast damn")
+    ib:AddLink("", "")
     ib:AddLink("Please share some food", "food")
     pl:Interface(1, ib:Build())
 end
 
 local function topicExplain()
     ib:SetTitle("About our Services")
-    ib:SetMsg("I will cast the named spells on you when you pay me the money.\nDeathsick is a stronger form of depletion.\nEverytime you die stats are depleted by death sickness.")
+    ib:SetMsg("\n\nI can teach you basic spells that you can cast yourself.\n\n")
+    ib:AddMsg("I can also cure various things by casting the named spells on you when you pay me the money.\n\n")
+    ib:AddMsg("Deathsick is a stronger form of depletion.\nEverytime you die stats are depleted by death sickness.")
     ib:SetButton("Back", "hi")
     pl:Interface(1, ib:Build())
 end
@@ -41,8 +51,7 @@ end
 
 local function topicCast(what)
     if what=="sick" then
-        ib:SetTitle("Casting Remove Death Sickness")
-        ib:SetMsg("I can cast °Remove Deathsick° for " .. pl:ShowCost(100 + (4 * pl. level * pl.level)))
+        ib:SetMsg("I can cast °Remove Deathsick° for " .. pl:ShowCost(100 + (4 * pl.level * pl.level)))
     elseif what == "deplete" then
         ib:SetMsg("I can cast °Remove Depletion° for ".. pl:ShowCost(5 * pl.level))
     elseif what == "restore" then
@@ -54,9 +63,9 @@ local function topicCast(what)
     elseif what == "curse" then
         ib:SetMsg("I can cast °Remove Curse° for ".. pl:ShowCost(100 * pl.level))
     else
-        ib:SetMsg("I can cast °Remove Damnation° for " .. pl:ShowCost(100 + (3 * pl. level * pl.level)))
+        ib:SetMsg("I can cast °Remove Damnation° for " .. pl:ShowCost(100 + (3 * pl.level * pl.level)))
     end
-    ib:AddMsg(".\n\nYou have " .. pl:ShowCost(pl:GetMoney()) .. ".\n\nYou want me to do it now?") 
+    ib:AddMsg(".\n\nYou have " .. pl:ShowCost(pl:GetMoney()) .. ".\n\nDo you want me to do it now?") 
     ib:SetAccept(nil, "docast " .. what) 
     ib:SetDecline(nil, "hi")
     pl:Interface(1, ib:Build())
@@ -64,7 +73,7 @@ end
 
 local function topicDoCast(what)
     if what == "sick" then
-        sum = 100 + (4 * pl. level * pl.level)
+        sum = 100 + (4 * pl.level * pl.level)
         spell = "remove death sickness"
     elseif what == "deplete" then
         sum = 5 * pl.level
@@ -79,10 +88,10 @@ local function topicDoCast(what)
         sum = 100 * pl.level
         spell = "cure disease"
     elseif what == "curse" then
-        sum = 5 * pl.level
+        sum = 100 * pl.level
         spell = "remove curse"
     else
-        sum = 100 + (3 * pl. level * pl.level)
+        sum = 100 + (3 * pl.level * pl.level)
         spell = "remove damnation"
     end
     ib:SetTitle("Casting " .. spell)
@@ -96,6 +105,55 @@ local function topicDoCast(what)
     pl:Interface(1, ib:Build())
 end
 
+local function topicTeach(what)
+    if what == "healing" then
+        ib:SetMsg("I can teach you °Minor Healing° for no charge.")
+    else
+        ib:SetMsg("I can teach you °Cause Light Wounds° for 2 silver.")
+        ib:AddMsg(".\n\nYou have " .. pl:ShowCost(pl:GetMoney()) .. ".")
+    end
+    ib:AddMsg("\n\nDo you want me to do it now?") 
+    ib:SetAccept(nil, "doteach " .. what) 
+    ib:SetDecline(nil, "hi")
+    pl:Interface(1, ib:Build())
+end
+
+local function topicDoTeach(what)
+    local ok = false
+    if what == "healing" then
+        spell = "minor healing"
+        ok = true
+    else
+        spell = "cause light wounds"
+    end
+    local spellno = game:GetSpellNr(spell)
+    if pl:DoKnowSpell(spellno) then
+        ib:SetMsg("You already know the spell '"..spell.."'!" )
+    else
+        if (what == "wounds") then
+            if pl:PayAmount(200) == 1 then
+                ok = true
+                ib:SetMsg("°** " .. me.name .. " takes your money **°\n\n")
+            else
+                ib:SetMsg("You don't have enough money!")
+            end
+        end
+
+        if ok then
+            ib:SetTitle("Teaching "..spell)
+            local skill = game:GetSkillNr("divine prayers")
+            if pl:FindSkill(skill) == nil then
+                ib:AddMsg("First I will teach you the skill 'divine prayers'\nYou need this skill to cast prayer spells.\n\n")
+                pl:AcquireSkill(skill, game.LEARN)
+            end
+            pl:AcquireSpell(spellno, game.LEARN)
+            ib:AddMsg("You learn the spell '"..spell.."'")
+        end
+    end
+    ib:SetButton("Back", "Hi") 
+    pl:Interface(1, ib:Build())
+end
+
 tl = TopicList()
 tl:AddGreeting(nil, topicDefault)
 tl:SetDefault(topicDefault)
@@ -103,4 +161,6 @@ tl:AddTopics("explain", topicExplain)
 tl:AddTopics("food", topicFood) 
 tl:AddTopics("cast (.*)", topicCast) 
 tl:AddTopics("docast (.*)", topicDoCast) 
+tl:AddTopics("teach (.*)", topicTeach) 
+tl:AddTopics("doteach (.*)", topicDoTeach) 
 tl:CheckMessage(event)
