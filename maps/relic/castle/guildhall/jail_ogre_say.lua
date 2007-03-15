e7e94eb258e927f667cedfcd6fb2e2921f6aa111
michtoen <michtoen@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-03-15 15:36:15 +0000

    removing old map directories

--- maps/relic/castle/guildhall/jail_ogre_say.lua
+++
@@ -1,4 +0,0 @@
--- make ogre spit when jail guard or some else tease him
-if string.lower(event.message) == "still there, frah'ak?" then
-	event.me:Communicate("/spit " .. event.activator.name)
-end
