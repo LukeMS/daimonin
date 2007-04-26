557555b04adac7f6fb5d0302c9f690369e109f77
michtoen <michtoen@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-04-26 01:03:23 +0000

    changed skill & spell giver scripts to GUI

--- trunk/maps/dev/testmaps/skills.lua
+++ trunk/maps/dev/testmaps/skills.lua
@@ -1,7 +1,18 @@
 require("topic_list");
+require("interface_builder")
 
-activator = event.activator
-whoami = event.me
+local pl 		= event.activator
+local me        = event.me
+local msg       = string.lower(event.message)
+
+local ib = InterfaceBuilder()
+ib:SetHeader(me, me.name)
+
+local function topicDefault()
+    ib:SetTitle("Add Skills to Player")
+    ib:AddMsg("I am the Skillgiver.\nType ^learn <skillname>^ or ^unlearn <skillname>^ (Note: unlearn skills is not implemented atm - the ideas of the daimonin skill system is to collect the skills - and don't lose them).")
+    pl:Interface(1, ib:Build())
+end
 
 tl = TopicList()
 
@@ -9,17 +20,18 @@ tl:AddTopics("learn (.*)",
     function(skillname)
         skill = game:GetSkillNr(skillname)
         if skill == -1 then
-            whoami:SayTo(activator,"Unknown skill \""..skillname.."\"" )
+            me:SayTo(pl,"Unknown skill \""..skillname.."\"" )
         else
-            if activator:FindSkill(skill) ~= nil then
-                whoami:SayTo(activator,"You already learned this skill." )
+            if pl:FindSkill(skill) ~= nil then
+                me:SayTo(pl,"You already learned this skill." )
             else
-                activator:AcquireSkill(skill, game.LEARN)
+                pl:AcquireSkill(skill, game.LEARN)
             end
         end
+        topicDefault()
     end
 )
-
-tl:SetDefault("\nI am the Skillgiver.\nSay ^learn <skillname>^ or ^unlearn <skillname>^ (Note: unlearn skills is not implemented atm - the ideas of the daimonin skill system is to collect the skills - and don't lose them).")
+tl:AddGreeting(nil, topicDefault)
+tl:SetDefault(topicDefault)
 
 tl:CheckMessage(event)
