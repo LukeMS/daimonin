cd9da339ade2d276e557d1737f742ad1bec394d8
neisklar <neisklar@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-05-18 22:27:19 +0000

    possibility to buy more than 1 piece at a time

--- trunk/maps/scripts/tavern.lua
+++ trunk/maps/scripts/tavern.lua
@@ -22,22 +22,27 @@ local function topicDefault()
     ib:SetTitle("Tavern")
     ib:SetMsg("Hello! I am " .. me.name ..".\n\nWelcome to "..tavern..".\nWe serve drinks and food here.\n\nWhat do you want?")
     for what, data in for_sale do
-        ib:AddLink("Buy "..data.title.. " for "..pl:ShowCost(data.price), "buy "..data.title)
+        ib:AddLink("Buy  1 "..data.title.. " for "..pl:ShowCost(data.price), "buy 1 "..data.title)
+        ib:AddLink("Buy 10 "..data.title.. " for "..pl:ShowCost(data.price*10), "buy 10 "..data.title)
+        ib:AddLink("Buy 25 "..data.title.. " for "..pl:ShowCost(data.price*25), "buy 25 "..data.title)
     end
     pl:Interface(1, ib:Build())
 end
 
-local function topicBuy(what)
+local function topicBuy(nrof, what)
     local data = for_sale[what]
     if data == nil then
         ib:SetTitle("Sorry, we don't have that")
         ib:SetMsg("Sorry, but we don't serve that here. Can I get you something else?")
         ib:SetDecline("No")
         ib:SetAccept("Yes", "hello")
-    else
+    else 
+    	if nrof == nil then 
+    	    nrof=1 
+    	end
         ib:SetTitle("Here is my offer")
-        ib:SetMsg("One ~"..data.title.."~ will cost you "..pl:ShowCost(data.price))    
-        ib:SetAccept(nil, "confirm "..data.title) 
+        ib:SetMsg(nrof.." ~"..data.title.."~ will cost you "..pl:ShowCost(data.price*nrof))    
+        ib:SetAccept(nil, "confirm "..nrof.." "..data.title) 
         ib:AddIcon(data.title, data.icon)
         ib:SetDecline(nil, "hi")
     end
@@ -45,17 +50,20 @@ local function topicBuy(what)
     pl:Interface(1, ib:Build())
 end
 
-local function topicConfirm(what)
+local function topicConfirm(nrof, what)
     ib:SetTitle("Buying some Water")
     local data = for_sale[what]
+    if nrof == nil then
+        nrof = 1
+    end
     if data == nil then
         ib:SetTitle("Sorry, we don't have that")
         ib:SetMsg("Sorry, but we don't serve that here. Can I get you something else?")
         ib:SetDecline("No")
         ib:SetAccept("Yes", "hello")
-    elseif pl:PayAmount(data.price) == 1 then
+    elseif pl:PayAmount(data.price*nrof) == 1 then
         ib:SetMsg("~** " .. me.name .. " takes your money **~\n\nOk, here is your "..data.title.."!")
-        tmp = pl:CreateObjectInsideEx(data.arch, 1, 1) 
+        tmp = pl:CreateObjectInsideEx(data.arch, 1, nrof) 
         ib:AddIcon(tmp.name, tmp:GetFace(), "")
     else
         ib:SetMsg("You don't have enough money!")
@@ -67,6 +75,6 @@ end
 tl = TopicList()
 tl:AddGreeting(nil, topicDefault)
 tl:SetDefault(topicDefault)
-tl:AddTopics("buy%s+(.*)", topicBuy) 
-tl:AddTopics("confirm%s+(.*)", topicConfirm) 
+tl:AddTopics("buy%s+(%d*)%s*(.*)", topicBuy) 
+tl:AddTopics("confirm%s+(%d*)%s*(.*)", topicConfirm) 
 tl:CheckMessage(event)
