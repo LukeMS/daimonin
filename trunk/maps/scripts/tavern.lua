f5ec41670d65b3e90ea2e56e7053e71b02dd2db2
swegecko <swegecko@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-05-18 12:11:11 +0000

    Generalize tavern script even more

--- trunk/maps/scripts/tavern.lua
+++ trunk/maps/scripts/tavern.lua
@@ -3,56 +3,60 @@ require("interface_builder")
 
 local pl = event.activator
 local me = event.me
-local msg = string.lower(event.message)
+local options = event.options
 
 local ib = InterfaceBuilder()
 ib:SetHeader(me, me.name)
 
+local tavern = "our tavern"
+if event.options then tavern = event.options end
+
+local for_sale = 
+{
+    water = {title = "water", price = 2, icon = "water.101", arch = "drink_generic"},
+    booze = {title = "booze", price = 7, icon = "booze.101", arch = "booze_generic"},
+    food =  {title = "food",  price =10, icon = "food.101", arch = "food_generic"}
+}
+
 local function topicDefault()
     ib:SetTitle("Tavern")
-    ib:SetMsg("Hello! I am " .. me.name ..".\n\nWelcome to our tavern.\nWe serve all kinds of booze here.\n\nWhat do you want?")
-    ib:AddLink("Buy water for 2 copper", "buy water")
-    ib:AddLink("Buy booze for 7 copper", "buy booze")
+    ib:SetMsg("Hello! I am " .. me.name ..".\n\nWelcome to "..tavern..".\nWe serve drinks and food here.\n\nWhat do you want?")
+    for what, data in for_sale do
+        ib:AddLink("Buy "..data.title.. " for "..pl:ShowCost(data.price), "buy "..data.title)
+    end
     pl:Interface(1, ib:Build())
 end
 
 local function topicBuy(what)
-    ib:SetTitle("Here is my offer")
-    if what=="water" then
-        ib:SetMsg("One °water° will cost you 2 copper")
-        ib:SetAccept(nil, "water") 
-        ib:AddIcon("water", "water.101", "")
+    local data = for_sale[what]
+    if data == nil then
+        ib:SetTitle("Sorry, we don't have that")
+        ib:SetMsg("Sorry, but we don't serve that here. Can I get you something else?")
+        ib:SetDecline("No")
+        ib:SetAccept("Yes", "hello")
     else
-        ib:SetMsg("One °booze° will cost you 7 copper")
-        ib:SetAccept(nil, "booze") 
-        ib:AddIcon("booze", "booze.101", "")
+        ib:SetTitle("Here is my offer")
+        ib:SetMsg("One ~"..data.title.."~ will cost you "..pl:ShowCost(data.price))    
+        ib:SetAccept(nil, "confirm "..data.title) 
+        ib:AddIcon(data.title, data.icon)
+        ib:SetDecline(nil, "hi")
     end
     ib:AddMsg(".\n\nYou have " .. pl:ShowCost(pl:GetMoney()) .. ".\n\nYou want to buy it?") 
-    ib:SetDecline(nil, "hi")
     pl:Interface(1, ib:Build())
 end
 
-local function topicWater()
+local function topicConfirm(what)
     ib:SetTitle("Buying some Water")
-    if pl:PayAmount(2) == 1 then
-        ib:SetMsg("°** " .. me.name .. " takes your money **°\n\nOk, here is your water!")
-        tmp = pl:CreateObjectInside("drink_generic", 1, 1) 
-        pl:Write('You got one water.', game.COLOR_WHITE); 
-        ib:AddIcon(tmp:GetName(), tmp:GetFace(), "")
-    else
-        ib:SetMsg("You don't have enough money!")
-    end
-    ib:SetButton("Back", "Hi") 
-    pl:Interface(1, ib:Build())
-end
-
-local function topicBooze()
-    ib:SetTitle("Buying some Booze")
-    if pl:PayAmount(7) == 1 then
-        ib:SetMsg("°** " .. me.name .. " takes your money **°\n\nOk, here is your booze!")
-        tmp = pl:CreateObjectInside("booze_generic", 1, 1) 
-        ib:AddIcon(tmp:GetName(), tmp:GetFace(), "")
-        pl:Write('You got one booze.', game.COLOR_WHITE); 
+    local data = for_sale[what]
+    if data == nil then
+        ib:SetTitle("Sorry, we don't have that")
+        ib:SetMsg("Sorry, but we don't server that here. Can I get you something else")
+        ib:SetDecline("No")
+        ib:SetAccept("Yes", "hello")
+    elseif pl:PayAmount(data.price) == 1 then
+        ib:SetMsg("~** " .. me.name .. " takes your money **~\n\nOk, here is your "..data.title.."!")
+        tmp = pl:CreateObjectInsideEx(data.arch, 1, 1) 
+        ib:AddIcon(tmp.name, tmp:GetFace(), "")
     else
         ib:SetMsg("You don't have enough money!")
     end
@@ -63,7 +67,6 @@ end
 tl = TopicList()
 tl:AddGreeting(nil, topicDefault)
 tl:SetDefault(topicDefault)
-tl:AddTopics("buy (.*)", topicBuy) 
-tl:AddTopics("water", topicWater) 
-tl:AddTopics("booze", topicBooze) 
+tl:AddTopics("buy%s+(.*)", topicBuy) 
+tl:AddTopics("confirm%s+(.*)", topicConfirm) 
 tl:CheckMessage(event)
