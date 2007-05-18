1835c44f597f3842e0b20aebbec057700b3d914e
rumbuff <rumbuff@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-05-18 23:27:09 +0000

    version 0.2.0 (first with GUI)

--- client3d/gui-editor/editor3d_start.bat
+++ client3d/gui-editor/editor3d_start.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java -classpath editor3d.jar;lib/commons-collections-3.2.jar;lib/commons-configuration-1.1.jar;lib/commons-lang-2.1.jar;lib/commons-logging-1.1.jar;lib/dom4j-1.6.1.jar;lib/jai_core.jar;lib/jai_codec.jar net.daimonin.client3d.editor.main.Editor3D %1 %2
+java -classpath editor3d.jar;lib/commons-collections-3.2.jar;lib/commons-configuration-1.1.jar;lib/commons-lang-2.1.jar;lib/commons-logging-1.1.jar;lib/dom4j-1.6.1.jar;lib/jai_core.jar;lib/jai_codec.jar net.daimonin.client3d.editor.main.Editor3D
