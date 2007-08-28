774753acfb67f8b04d7eedf6882d11f25dda2ae1
neisklar <neisklar@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-08-28 16:42:48 +0000

    channel-system: adding (deactivated) small announcement for channels at login

--- server/src/include/config.h
+++ server/src/include/config.h
@@ -50,7 +50,8 @@
 /* Channelsystem: uncomment this line to compile it in */
 /* you HAVE to also enable this in the lua-plugin (plugin_lua.h)!!! */
 //#define USE_CHANNELS
-
+/* when defined, annouces the system at login */
+//#define ANNOUNCE_CHANNELS
 
 /* Location of read-only machine independent data */
 #ifndef DATADIR
