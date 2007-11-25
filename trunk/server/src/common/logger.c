5fe3e354bde767c3eefdafd7ba5f7f8b3b7de760
jeffg1 <jeffg1@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2007-11-25 20:23:55 +0000

    Add timestamps to log at 10 minute intervals.

--- trunk/server/src/common/logger.c
+++ trunk/server/src/common/logger.c
@@ -27,6 +27,65 @@
 
 static char log_buf[MAXSOCKBUF*2];
 
+/* minimum timestamp interval = 10 minutes */
+#define TIMESTAMP_INTERVAL 600
+static struct timeval last_timestamp = {0, 0};
+
+/* Pull out common printing code from LOG */
+void do_print(char *buf)
+{
+#ifdef WIN32 /* ---win32 change log handling for win32 */
+    if (logfile)
+        fputs(buf, logfile);    /* wrote to file or stdout */
+    else
+        fputs(buf, stderr);
+
+#ifdef DEBUG                /* if we have a debug version, we want see ALL output */
+    if (logfile)
+        fflush(logfile);    /* so flush this! We need this because we don't have added a exception/signal handler for win32 */
+#endif
+    if (logfile && logfile != stderr)   /* if was it a logfile wrote it to screen too */
+        fputs(buf, stderr);
+#else
+    if (logfile)
+        fputs(buf, logfile);
+    else
+        fputs(buf, stderr);
+#endif
+}
+
+/* Check if timestamp is due */
+void check_timestamp()
+{
+    struct timeval   now;
+    struct tm       *tim;
+    char             buf[256];
+
+    GETTIMEOFDAY(&now);
+    if (now.tv_sec >= (last_timestamp.tv_sec + TIMESTAMP_INTERVAL))
+    {
+        last_timestamp.tv_sec = now.tv_sec;
+        tim = localtime(&now.tv_sec);
+        sprintf(buf, "TIMESTAMP: %4d-%02d-%02d %02d:%02d:%02d\n",
+            tim->tm_year+1900, tim->tm_mon+1, tim->tm_mday,
+            tim->tm_hour, tim->tm_min, tim->tm_sec);
+        do_print(buf);
+    }
+}
+
+/* force_timestamp can be called to force a timestamp on the
+ * next logged message, regardless of the time since the last
+ * one. This could be used for an event where an
+ * accurate time of occurrence is needed. For example, call
+ * this before logging a player login/logout if required.
+ * The call could be enclosed in a #ifdef to control this
+ * with a #define, of course.
+ */
+ void force_timestamp()
+ {
+     last_timestamp.tv_sec = 0;
+ }
+
 /*
  * Logs a message to stderr, or to file, and/or even to socket.
  * Or discards the message if it is of no importanse, and none have
@@ -43,28 +102,15 @@ void LOG(LogLevel logLevel, char *format, ...)
     va_list     ap;
     va_start(ap, format);
 
+    /* Check if timestamp needed */
+    check_timestamp();
+
     log_buf[0] = '\0';
     if (logLevel <= settings.debug)
     {
         vsprintf(log_buf, format, ap);
-#ifdef WIN32 /* ---win32 change log handling for win32 */
-        if (logfile)
-            fputs(log_buf, logfile);    /* wrote to file or stdout */
-        else
-            fputs(log_buf, stderr);
+        do_print(log_buf);
 
-#ifdef DEBUG                /* if we have a debug version, we want see ALL output */
-        if (logfile)
-            fflush(logfile);    /* so flush this! We need this because we don't have added a exception/signal handler for win32 */
-#endif
-        if (logfile && logfile != stderr)   /* if was it a logfile wrote it to screen too */
-            fputs(log_buf, stderr);
-#else
-        if (logfile)
-            fputs(log_buf, logfile);
-        else
-            fputs(log_buf, stderr);
-#endif
         /* Mapbugs are broadcasted on the test server */
 
 #ifdef _TESTSERVER
