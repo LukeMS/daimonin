010651710a9c2874a8982bca967b9fcd4cb0e733
tehill <tehill@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2008-05-04 02:09:16 +0000

    added materials to stone,paper and bone

--- trunk/server/src/common/treasure.c
+++ trunk/server/src/common/treasure.c
@@ -2383,7 +2383,7 @@ static inline void set_material_real(object *op, struct _change_arch *change_arc
                      * At this point we exclude the different used tables
                      */
             /* we should add paper & cloth here too later */
-            if (m_tmp == M_START_IRON || m_tmp == M_START_WOOD || m_tmp == M_START_LEATHER || m_tmp == M_START_CLOTH)
+            if (m_tmp == M_START_IRON || m_tmp == M_START_WOOD || m_tmp == M_START_LEATHER || m_tmp == M_START_CLOTH || m_tmp == M_START_STONE)
             {
                 for (i = 0; i < NROFMATERIALS_REAL; i++)
                 {
