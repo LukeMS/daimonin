3866e9ab6f87d8316eac82ca73eaebac06eef192
jeffg1 <jeffg1@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2008-05-11 15:02:54 +0000

    Wrap function with validity check. It's possible for a script to lose the player (believe me, I've done it!), and for this to be called with a nil activator. The server then goes into an infinite loop.

--- trunk/maps/lua/event_logout.lua
+++ trunk/maps/lua/event_logout.lua
@@ -7,15 +7,21 @@ local pl = event.activator
 -- TODO: this would be a nice place to unload the datastore, but
 -- it might still be referenced by some script, so we can't do that.
 
-if pl.experience == 0 then
-    -- Don't save data for unused characters.
-    -- Also, we _must_ delete the data from memory, since the character 
-    -- name will be reused. We just hope no script is still referencing it.
-    _data_store.forget_player(pl.name)
-else
-    -- For safety we save real player's datastore when they
-    -- log out. It would be nice to be able to free it here too.
-    -- Maybe we can move it over to a weak table?
-    _data_store.save_player(pl.name, true)
+-- Grommit 11-May-2008: wrap with validity check. It's possible for a script
+-- to lose the player (believe me, I've done it!), and for this to be called
+-- with a nil activator. The server then goes into an infinite loop.
+
+if game:IsValid(pl) then
+    if pl.experience == 0 then
+        -- Don't save data for unused characters.
+        -- Also, we _must_ delete the data from memory, since the character
+        -- name will be reused. We just hope no script is still referencing it.
+        _data_store.forget_player(pl.name)
+    else
+        -- For safety we save real player's datastore when they
+        -- log out. It would be nice to be able to free it here too.
+        -- Maybe we can move it over to a weak table?
+        _data_store.save_player(pl.name, true)
+    end
 end
 
