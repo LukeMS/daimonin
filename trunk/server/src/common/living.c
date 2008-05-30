a914d5dfaa5e00729a8751f00f25d0c5befd2561
longir <longir@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2008-05-30 00:46:07 +0000

    setting stat penalties in proper order

--- trunk/server/src/common/living.c
+++ trunk/server/src/common/living.c
@@ -57,7 +57,7 @@ _races          item_race_table[RACE_NAME_INIT] =
 #define ENCUMBRANCE_LIMIT 0.35f
 
 /* for atat values 0 to 9 */
-float stats_penalty[10] = {0.9f, 0.85f, 0.8f, 0.7f, 0.6f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f};
+float stats_penalty[10] = {0.1f, 0.15f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f};
 
 static char    *drain_msg[7]                    =
 {
@@ -2040,7 +2040,7 @@ void dragon_level_gain(object *who)
 void fix_monster(object *op)
 {
     int wc_mali=0, ac_mali=0, snare_penalty=0, slow_penalty=0;
-    object *base, *tmp, *spawn_info=NULL, *bow=NULL, wc_tmp;
+    object *base, *tmp, *spawn_info=NULL, *bow=NULL;
     float   tmp_add;
     int mob_wc[MAXLEVEL + 1] = 
      { 1,2,3,4,5,6,7,8,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17,18,
@@ -2185,7 +2185,7 @@ void fix_monster(object *op)
     /* + level/10 to catch up the equipment improvements of the players. */
     op->stats.ac = base->stats.ac + (op->level / 10) - ac_mali;
     op->stats.wc = base->stats.wc + (op->level / 10) - wc_mali;
-    wc_tmp = op->arch->clone;
+    object wc_tmp = op->arch->clone;
     if(op->stats.wc == (wc_tmp.stats.wc + ((int)op->level / 10)))
           op->stats.wc = mob_wc[op->level];
     if(op->level < 10)
