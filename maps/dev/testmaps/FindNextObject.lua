414bdb3e7aa44a9e60b2fe98443cb1229c0bcd9e
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2008-08-15 16:28:27 +0000

    Fix for latest object:FNO().

--- maps/dev/testmaps/FindNextObject.lua
+++ maps/dev/testmaps/FindNextObject.lua
@@ -7,10 +7,5 @@ if nextobj == nil then
     nextobj = player
 end
 
-nextobj = nextobj:FindNextObject(game.TYPE_CONTAINER, nil, nil, nil, game.FNO_MODE_CONTAINERS)
-if nextobj then
-    message = nextobj.name .. "[" .. nextobj.count .. "]"
-else
-    message = "nil"
-end
-player:Write("Next object is " .. message .. "!", game.COLOR_RED)
+nextobj = nextobj:FindNextObject(game.TYPE_CONTAINER, game.FNO_MODE_CONTAINERS)
+player:Write("Next object is " .. tostring(nextobj) .. "!", game.COLOR_RED)
