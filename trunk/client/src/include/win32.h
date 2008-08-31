0242a2d321b6343f0f8d39089b2321bd6a4ba5bf
michtoen <michtoen@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2008-08-31 23:17:20 +0000

    setting trunk to version 0.10.0, default patch level 200

--- trunk/client/src/include/win32.h
+++ trunk/client/src/include/win32.h
@@ -94,16 +94,16 @@
 #define PACKAGE_NAME "Daimonin SDL Client"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Daimonin SDL Client 0.9.7.1"
+#define PACKAGE_STRING "Daimonin SDL Client 0.10.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "daimonin-sdl-client"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "0.9.7.1"
+#define PACKAGE_VERSION "0.10.0"
 
 /* Installation prefix */
-#define PREFIX "../../../client-0.9.7.1"
+#define PREFIX "../../../client-0.10.0"
 
 /* Use the SDL_mixer sound system. Remove when you have no sound card or slow
    computer */
