1f2e1be6a2b902d74560109ad0bf0c1ae9c5e9eb
jeffg1 <jeffg1@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2008-09-06 10:35:35 +0000

    Fix signed/unsigned issues

--- server/src/include/object.h
+++ server/src/include/object.h
@@ -33,7 +33,7 @@
 #define decrease_ob(xyz) decrease_ob_nr(xyz,1)
 
 #define WEIGHT(op) ((op->nrof ? op->weight * op->nrof : (unsigned int) (op->weight) + op->carrying))
-#define WEIGHT_NROF(op, nrof) ((nrof ? op->weight * nrof : op->weight) + op->carrying)
+#define WEIGHT_NROF(op, nrof) (((nrof > 0) ? op->weight * ((sint32)nrof) : op->weight) + op->carrying)
 
 /* move_apply() function call flags */
 #define MOVE_APPLY_DEFAULT  0
