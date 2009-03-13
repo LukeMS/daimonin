2db985ed3fc7bc8c261c6ec05c3f0b494d94989d
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2009-03-13 12:29:27 +0000

    Consistently use is_magical macro.

--- trunk/server/src/common/view_inv.c
+++ trunk/server/src/common/view_inv.c
@@ -114,7 +114,7 @@ unsigned int query_flags(object *op)
         else if (QUERY_FLAG(op, FLAG_CURSED))
             flags |= F_CURSED;
     }
-    if (QUERY_FLAG(op, FLAG_KNOWN_MAGICAL) && QUERY_FLAG(op, FLAG_IS_MAGICAL))
+    if (QUERY_FLAG(op, FLAG_KNOWN_MAGICAL) && is_magical(op))
         flags |= F_MAGIC;
     if (QUERY_FLAG(op, FLAG_UNPAID))
         flags |= F_UNPAID;
