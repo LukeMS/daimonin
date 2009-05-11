ef0b63a99dcbfeabe68ee8fff2fdfb0c07296510
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2009-05-11 12:38:38 +0000

    Load map4 new, which does cause current equality test to fail.

--- maps/dev/testmaps/mapeq.lua
+++ maps/dev/testmaps/mapeq.lua
@@ -6,15 +6,15 @@ local map2 = player.map
 if map1 == map2 then
     player:Write("map1 == map2! :)")
 else
-    player:Write("map1 ~= map2! :(")
+    player:Write("map1 != map2! :(")
 end
 
 local map3 = player.map
-local map4 = game:ReadyMap(player.map.path)
+local map4 = game:ReadyMap(player.map.path, game.MAP_NEW)
 
 if map3 == map4 then
     player:Write("map3 == map4! :)")
 else
-    player:Write("map3 ~= map4! :(")
+    player:Write("map3 != map4! :(")
 end
 
