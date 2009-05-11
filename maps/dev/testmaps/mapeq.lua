da9e0239d42d46ea0986b2d747f2db8a5d5891e4
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2009-05-11 12:11:38 +0000

    Quick test of current map equality for dev server.

---
+++ maps/dev/testmaps/mapeq.lua
@@ -0,0 +1,20 @@
+local player = event.activator
+
+local map1 = player.map
+local map2 = player.map
+
+if map1 == map2 then
+    player:Write("map1 == map2! :)")
+else
+    player:Write("map1 ~= map2! :(")
+end
+
+local map3 = player.map
+local map4 = game:ReadyMap(player.map.path)
+
+if map3 == map4 then
+    player:Write("map3 == map4! :)")
+else
+    player:Write("map3 ~= map4! :(")
+end
+
