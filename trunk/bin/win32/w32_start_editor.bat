7a3f22569f09fc3d3edd5b26f5f6211faba9e200
lippy1 <lippy1@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2009-07-13 23:11:07 +0000

    Add functionality to patch the editor after an update is downloaded. This is because in practical terms it is impossible for the editor to patch itself on Windows, due to file locking, and it being a single file solution.
    Also, the functionality in this file used to exist in the old mapmakers, but for some reason was never committed to the public tree.

--- trunk/bin/win32/w32_start_editor.bat
+++ trunk/bin/win32/w32_start_editor.bat
@@ -1,4 +1,7 @@
 REM *** this should start the editor **
 cd ..\..\editor
+IF EXIST DaimoninEditor.jar.tmp (
+rename DaimoninEditor.jar DaimoninEditor.jar.bak
+rename DaimoninEditor.jar.tmp DaimoninEditor.jar)
 javaw.exe -jar DaimoninEditor.jar
 cd ..\bin\win32
