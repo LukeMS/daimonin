7522a5463414a37c7c0a9f53b92f7e4afbb2c0e6
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2009-07-25 13:47:06 +0000

    MWs don't need /summon.

--- trunk/server/src/socket/commands.c
+++ trunk/server/src/socket/commands.c
@@ -200,7 +200,6 @@ CommArray_s CommandsGM[] =
 
 CommArray_s CommandsMW[] =
 {
-    {"summon",        command_summon,      0.0f, 1},
     {"teleport",      command_teleport,    0.0f, 1},
     {"resetmap",      command_reset,       0.0f, 1},
     {"goto",          command_goto,        0.0f, 1},
