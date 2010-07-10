b28d5c80f12929c7f31cb56b36bcec46dd4aa893
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2010-07-10 14:10:27 +0000

    Improve efficiency and tidyups.

--- server/src/common/info.c
+++ server/src/common/info.c
@@ -32,10 +32,10 @@
  */
 #include "global.h"
 
-static void NewDrawInfo(const int flags, player *pl, const char *const buf);
 static void NewInfoMapAllExcept(const int flags, const mapstruct *const map,
                                 const object *const op1, const object *const op,
                                 const char *const str);
+static void NewDrawInfo(int flags, player *pl, const char *const buf);
 
 /* new_draw_info:
  *
@@ -77,11 +77,9 @@ void new_draw_info(const int flags, const int pri, const object *const op,
 
             for (; pl; pl = pl->next)
             {
-                if (!(pl->state & (ST_DEAD | ST_ZOMBIE)) &&
-                    pl->socket.status != Ns_Dead &&
-                    pri <= pl->listening)
+                if (pri <= pl->listening)
                 {
-                    NewDrawInfo((flags & ~NDI_ALL), pl, buf);
+                    NewDrawInfo(flags, pl, buf);
                 }
             }
         }
@@ -96,7 +94,6 @@ void new_draw_info(const int flags, const int pri, const object *const op,
             if (op &&
                 op->type == PLAYER &&
                 CONTR(op) &&
-                (CONTR(op)->state & ST_PLAYING) &&
                 pri <= CONTR(op)->listening)
             {
                 NewDrawInfo(flags, CONTR(op), buf);
@@ -130,6 +127,7 @@ void new_info_map(const int flags, const mapstruct *const map, const int x,
     {
         return;
     }
+
     if (dist == MAP_INFO_ALL)
     {
         NewInfoMapAllExcept(flags, map, NULL, NULL, buf); /* we want all on this map */
@@ -144,7 +142,9 @@ void new_info_map(const int flags, const mapstruct *const map, const int x,
         for (tmp = map->player_first; tmp; tmp = CONTR(tmp)->map_above)
         {
             if ((POW2(tmp->x - x) + POW2(tmp->y - y)) <= d)
+            {
                 NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
     }
 
@@ -281,34 +281,6 @@ void new_info_map(const int flags, const mapstruct *const map, const int x,
     }
 }
 
-static void NewDrawInfo(const int flags, player *pl, const char *const buf)
-{
-    NewSocket    *ns = &pl->socket;
-    const size_t  len = strlen(buf) + 3;
-
-    SOCKBUF_REQUEST_BUFFER(ns, len);
-    SockBuf_AddShort(ACTIVE_SOCKBUF(ns), (flags & NDI_FLAG_MASK));
-    SockBuf_AddString(ACTIVE_SOCKBUF(ns), buf, len);
-    SOCKBUF_REQUEST_FINISH(ns, BINARY_CMD_DRAWINFO2, SOCKBUF_DYNAMIC);
-}
-
-/* we want give msg to all people on one, specific map. */
-static void NewInfoMapAllExcept(const int flags, const mapstruct *const map,
-                                const object *const op1,
-                                const object *const op, const char *const str)
-{
-    object *tmp;
-
-    if (map->player_first)
-    {
-        for (tmp = map->player_first; tmp; tmp = CONTR(tmp)->map_above)
-        {
-            if (tmp != op && tmp != op1)
-                new_draw_info(flags, 0, tmp, str);
-        }
-    }
-}
-
 /* write to everyone on the map *except* op and op1.  This is useful for emotions. */
 void new_info_map_except(const int flags, const mapstruct *const map,
                          const int x, const int y, const int dist,
@@ -335,14 +307,20 @@ void new_info_map_except(const int flags, const mapstruct *const map,
     va_end(ap);
     buf[sizeof(buf) - 1] = '\0';
 
-    if (!map || map->in_memory != MAP_IN_MEMORY)
+    if (!map ||
+         map->in_memory != MAP_IN_MEMORY)
+    {
         return;
+    }
 
     if (dist != MAP_INFO_ALL)
+    {
         d = POW2(dist);
+    }
     else
     {
         NewInfoMapAllExcept(flags, map, op1, op, buf); /* we want all on this map */
+
         return;
     }
 
@@ -350,85 +328,198 @@ void new_info_map_except(const int flags, const mapstruct *const map,
     {
         for (tmp = map->player_first; tmp; tmp = CONTR(tmp)->map_above)
         {
-            if (tmp != op && tmp != op1 && (POW2(tmp->x - x) + POW2(tmp->y - y)) <= d)
-                new_draw_info(flags, 0, tmp, buf);
+            if (tmp != op &&
+                tmp != op1 &&
+                (POW2(tmp->x - x) + POW2(tmp->y - y)) <= d)
+            {
+                NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
     }
 
-    if (map->tile_map[TILED_MAPS_NORTH] && map->tile_map[TILED_MAPS_NORTH]->in_memory == MAP_IN_MEMORY && map->tile_map[TILED_MAPS_NORTH]->player_first)
+    if (map->tile_map[TILED_MAPS_NORTH] &&
+        map->tile_map[TILED_MAPS_NORTH]->in_memory == MAP_IN_MEMORY &&
+        map->tile_map[TILED_MAPS_NORTH]->player_first)
     {
         yt = y + MAP_HEIGHT(map->tile_map[TILED_MAPS_NORTH]);
-        for (tmp = map->tile_map[TILED_MAPS_NORTH]->player_first; tmp; tmp = CONTR(tmp)->map_above)
+
+        for (tmp = map->tile_map[TILED_MAPS_NORTH]->player_first; tmp;
+             tmp = CONTR(tmp)->map_above)
         {
-            if (tmp != op && tmp != op1 && (POW2(tmp->x - x) + POW2(tmp->y - yt)) <= d)
-                new_draw_info(flags, 0, tmp, buf);
+            if (tmp != op &&
+                tmp != op1 &&
+                (POW2(tmp->x - x) + POW2(tmp->y - yt)) <= d)
+            {
+                NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
     }
-    if (map->tile_map[TILED_MAPS_EAST] && map->tile_map[TILED_MAPS_EAST]->in_memory == MAP_IN_MEMORY && map->tile_map[TILED_MAPS_EAST]->player_first)
+
+    if (map->tile_map[TILED_MAPS_EAST] &&
+        map->tile_map[TILED_MAPS_EAST]->in_memory == MAP_IN_MEMORY &&
+        map->tile_map[TILED_MAPS_EAST]->player_first)
     {
         xt = x - MAP_WIDTH(map);
-        for (tmp = map->tile_map[TILED_MAPS_EAST]->player_first; tmp; tmp = CONTR(tmp)->map_above)
+
+        for (tmp = map->tile_map[TILED_MAPS_EAST]->player_first; tmp;
+             tmp = CONTR(tmp)->map_above)
         {
-            if (tmp != op && tmp != op1 && (POW2(tmp->x - xt) + POW2(tmp->y - y)) <= d)
-                new_draw_info(flags, 0, tmp, buf);
+            if (tmp != op &&
+                tmp != op1 &&
+                (POW2(tmp->x - xt) + POW2(tmp->y - y)) <= d)
+            {
+                NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
     }
-    if (map->tile_map[TILED_MAPS_SOUTH] && map->tile_map[TILED_MAPS_SOUTH]->in_memory == MAP_IN_MEMORY && map->tile_map[TILED_MAPS_SOUTH]->player_first)
+
+    if (map->tile_map[TILED_MAPS_SOUTH] &&
+        map->tile_map[TILED_MAPS_SOUTH]->in_memory == MAP_IN_MEMORY &&
+        map->tile_map[TILED_MAPS_SOUTH]->player_first)
     {
         yt = y - MAP_HEIGHT(map);
-        for (tmp = map->tile_map[TILED_MAPS_SOUTH]->player_first; tmp; tmp = CONTR(tmp)->map_above)
+
+        for (tmp = map->tile_map[TILED_MAPS_SOUTH]->player_first; tmp;
+             tmp = CONTR(tmp)->map_above)
         {
-            if (tmp != op && tmp != op1 && (POW2(tmp->x - x) + POW2(tmp->y - yt)) <= d)
-                new_draw_info(flags, 0, tmp, buf);
+            if (tmp != op &&
+                tmp != op1 &&
+                (POW2(tmp->x - x) + POW2(tmp->y - yt)) <= d)
+            {
+                NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
     }
-    if (map->tile_map[TILED_MAPS_WEST] && map->tile_map[TILED_MAPS_WEST]->in_memory == MAP_IN_MEMORY && map->tile_map[TILED_MAPS_WEST]->player_first)
+
+    if (map->tile_map[TILED_MAPS_WEST] &&
+        map->tile_map[TILED_MAPS_WEST]->in_memory == MAP_IN_MEMORY &&
+        map->tile_map[TILED_MAPS_WEST]->player_first)
     {
         xt = x + MAP_WIDTH(map->tile_map[TILED_MAPS_WEST]);
-        for (tmp = map->tile_map[TILED_MAPS_WEST]->player_first; tmp; tmp = CONTR(tmp)->map_above)
+
+        for (tmp = map->tile_map[TILED_MAPS_WEST]->player_first; tmp;
+             tmp = CONTR(tmp)->map_above)
         {
-            if (tmp != op && tmp != op1 && (POW2(tmp->x - xt) + POW2(tmp->y - y)) <= d)
-                new_draw_info(flags, 0, tmp, buf);
+            if (tmp != op &&
+                tmp != op1 &&
+                (POW2(tmp->x - xt) + POW2(tmp->y - y)) <= d)
+            {
+                NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
     }
-    if (map->tile_map[TILED_MAPS_NORTHEAST] && map->tile_map[TILED_MAPS_NORTHEAST]->in_memory == MAP_IN_MEMORY && map->tile_map[TILED_MAPS_NORTHEAST]->player_first)
+
+    if (map->tile_map[TILED_MAPS_NORTHEAST] &&
+        map->tile_map[TILED_MAPS_NORTHEAST]->in_memory == MAP_IN_MEMORY &&
+        map->tile_map[TILED_MAPS_NORTHEAST]->player_first)
     {
         yt = y + MAP_HEIGHT(map->tile_map[TILED_MAPS_NORTHEAST]);
         xt = x - MAP_WIDTH(map);
-        for (tmp = map->tile_map[TILED_MAPS_NORTHEAST]->player_first; tmp; tmp = CONTR(tmp)->map_above)
+
+        for (tmp = map->tile_map[TILED_MAPS_NORTHEAST]->player_first; tmp;
+             tmp = CONTR(tmp)->map_above)
         {
-            if (tmp != op && tmp != op1 && (POW2(tmp->x - xt) + POW2(tmp->y - yt)) <= d)
-                new_draw_info(flags, 0, tmp, buf);
+            if (tmp != op &&
+                tmp != op1 &&
+                (POW2(tmp->x - xt) + POW2(tmp->y - yt)) <= d)
+            {
+                NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
     }
-    if (map->tile_map[TILED_MAPS_SOUTHEAST] && map->tile_map[TILED_MAPS_SOUTHEAST]->in_memory == MAP_IN_MEMORY && map->tile_map[TILED_MAPS_SOUTHEAST]->player_first)
+
+    if (map->tile_map[TILED_MAPS_SOUTHEAST] &&
+        map->tile_map[TILED_MAPS_SOUTHEAST]->in_memory == MAP_IN_MEMORY &&
+        map->tile_map[TILED_MAPS_SOUTHEAST]->player_first)
     {
         xt = x - MAP_WIDTH(map);
         yt = y - MAP_HEIGHT(map);
-        for (tmp = map->tile_map[TILED_MAPS_SOUTHEAST]->player_first; tmp; tmp = CONTR(tmp)->map_above)
+
+        for (tmp = map->tile_map[TILED_MAPS_SOUTHEAST]->player_first; tmp;
+             tmp = CONTR(tmp)->map_above)
         {
-            if (tmp != op && tmp != op1 && (POW2(tmp->x - xt) + POW2(tmp->y - yt)) <= d)
-                new_draw_info(flags, 0, tmp, buf);
+            if (tmp != op &&
+                tmp != op1 &&
+                (POW2(tmp->x - xt) + POW2(tmp->y - yt)) <= d)
+            {
+                NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
     }
-    if (map->tile_map[TILED_MAPS_SOUTHWEST] && map->tile_map[TILED_MAPS_SOUTHWEST]->in_memory == MAP_IN_MEMORY && map->tile_map[TILED_MAPS_SOUTHWEST]->player_first)
+
+    if (map->tile_map[TILED_MAPS_SOUTHWEST] &&
+        map->tile_map[TILED_MAPS_SOUTHWEST]->in_memory == MAP_IN_MEMORY &&
+        map->tile_map[TILED_MAPS_SOUTHWEST]->player_first)
     {
         xt = x + MAP_WIDTH(map->tile_map[TILED_MAPS_SOUTHWEST]);
         yt = y - MAP_HEIGHT(map);
-        for (tmp = map->tile_map[TILED_MAPS_SOUTHWEST]->player_first; tmp; tmp = CONTR(tmp)->map_above)
+
+        for (tmp = map->tile_map[TILED_MAPS_SOUTHWEST]->player_first; tmp;
+             tmp = CONTR(tmp)->map_above)
         {
-            if (tmp != op && tmp != op1 && (POW2(tmp->x - xt) + POW2(tmp->y - yt)) <= d)
-                new_draw_info(flags, 0, tmp, buf);
+            if (tmp != op &&
+                tmp != op1 &&
+                (POW2(tmp->x - xt) + POW2(tmp->y - yt)) <= d)
+            {
+                NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
     }
-    if (map->tile_map[TILED_MAPS_NORTHWEST] && map->tile_map[TILED_MAPS_NORTHWEST]->in_memory == MAP_IN_MEMORY && map->tile_map[TILED_MAPS_NORTHWEST]->player_first)
+
+    if (map->tile_map[TILED_MAPS_NORTHWEST] &&
+        map->tile_map[TILED_MAPS_NORTHWEST]->in_memory == MAP_IN_MEMORY &&
+        map->tile_map[TILED_MAPS_NORTHWEST]->player_first)
     {
         xt = x + MAP_WIDTH(map->tile_map[TILED_MAPS_NORTHWEST]);
         yt = y + MAP_HEIGHT(map->tile_map[TILED_MAPS_NORTHWEST]);
-        for (tmp = map->tile_map[TILED_MAPS_NORTHWEST]->player_first; tmp; tmp = CONTR(tmp)->map_above)
+
+        for (tmp = map->tile_map[TILED_MAPS_NORTHWEST]->player_first; tmp;
+             tmp = CONTR(tmp)->map_above)
         {
-            if (tmp != op && tmp != op1 && (POW2(tmp->x - xt) + POW2(tmp->y - yt)) <= d)
-                new_draw_info(flags, 0, tmp, buf);
+            if (tmp != op &&
+                tmp != op1 &&
+                (POW2(tmp->x - xt) + POW2(tmp->y - yt)) <= d)
+            {
+                NewDrawInfo(flags, CONTR(tmp), buf);
+            }
         }
+    }
+}
+
+/* we want give msg to all people on one, specific map. */
+static void NewInfoMapAllExcept(const int flags, const mapstruct *const map,
+                                const object *const op1,
+                                const object *const op, const char *const str)
+{
+    object *tmp;
+
+    if (map->player_first)
+    {
+        for (tmp = map->player_first; tmp; tmp = CONTR(tmp)->map_above)
+        {
+            if (tmp != op &&
+                tmp != op1)
+            {
+                NewDrawInfo(flags, CONTR(tmp), str);
+            }
+        }
+    }
+}
+
+static void NewDrawInfo(int flags, player *pl, const char *const buf)
+{
+    if (!(pl->state & (ST_DEAD | ST_ZOMBIE)) &&
+        pl->socket.status != Ns_Dead)
+    {
+        NewSocket    *ns = &pl->socket;
+        const size_t  len = strlen(buf) + 3;
+
+        /* Turn off any internal flags. */
+        flags &= ~NDI_ALL;
+
+        SOCKBUF_REQUEST_BUFFER(ns, len);
+        SockBuf_AddShort(ACTIVE_SOCKBUF(ns), (flags & NDI_FLAG_MASK));
+        SockBuf_AddString(ACTIVE_SOCKBUF(ns), buf, len);
+        SOCKBUF_REQUEST_FINISH(ns, BINARY_CMD_DRAWINFO2, SOCKBUF_DYNAMIC);
     }
 }
