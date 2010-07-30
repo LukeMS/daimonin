8ded33f1a2f179e35b02ab6e347c0a76bf94ca8c
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2010-07-30 21:02:42 +0000

    Client fix from r5731 to server too.

--- trunk/server/src/include/global.h
+++ trunk/server/src/include/global.h
@@ -105,18 +105,42 @@ typedef signed long long   sint64;
 
 /* mallocs _P_ to size _S_, logging OOM or initialising to 0. */
 #undef MALLOC
+#if 0 // Seems Visual C++ cannot handle this style.
 #define MALLOC(_P_, _S_) \
 ((!((_P_) = malloc((_S_)))) ? \
 LOG(llevBug, "BUG:: %s %d: Out of memory!\n", __FILE__, __LINE__) : \
 memset((_P_), 0, (_S_)))
+#else
+#define MALLOC(_P_, _S_) \
+if (!((_P_) = malloc((_S_)))) \
+{ \
+    LOG(llevBug, "BUG:: %s %d: Out of memory!\n", __FILE__, __LINE__); \
+} \
+else \
+{ \
+    memset((_P_), 0, (_S_)); \
+}
+#endif
 
 /* mallocs _P_ to size strlen(_S_) + 1, logging OOM or initialising to
  * sprintf(_P_, "%s", _S_). */
 #undef MALLOC2
+#if 0 // Seems Visual C++ cannot handle this style.
 #define MALLOC2(_P_, _S_) \
-((!((_P_) = malloc(strlen(_S_) + 1))) ? \
+((!((_P_) = malloc(strlen((_S_)) + 1))) ? \
 LOG(llevBug, "BUG:: %s %d: Out of memory!\n", __FILE__, __LINE__) : \
 sprintf((_P_), "%s", (_S_)))
+#else
+#define MALLOC2(_P_, _S_) \
+if (!((_P_) = malloc(strlen((_S_)) + 1))) \
+{ \
+    LOG(llevBug, "BUG:: %s %d: Out of memory!\n", __FILE__, __LINE__); \
+} \
+else \
+{ \
+    sprintf((_P_), "%s", (_S_)); \
+}
+#endif
 
 /* frees _P_ and sets it to NULL. */
 #undef FREE
