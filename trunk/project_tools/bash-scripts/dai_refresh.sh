e058e3bf94342671ba1479bed37b1b7e93afbd01
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2010-08-04 14:50:56 +0000

    Bash scripts to maintain a gameserver.

---
+++ trunk/project_tools/bash-scripts/dai_refresh.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Kill gameserver, and update scripts.
+
+########
+# Set global variables.
+. ./dai_setvars.sh
+
+if [ -f ./nohup.out ]
+then
+    mv ./nohup.out logs/`date +%Y%m%d_%H%M%S%Z --utc`.log
+fi
+
+echo
+echo "#######################"
+echo "### Kill gameserver ###"
+echo "#######################"
+
+ps ax | grep "${dai_server_exe}" | grep -v grep | awk '{print $1}' | xargs kill > /dev/null
+ps ax | grep "${dai_reboot_sh}" | grep -v grep | awk '{print $1}' | xargs kill > /dev/null
+sleep 15
+
+echo
+echo "#############################"
+echo "### Direct export scripts ###"
+echo "#############################"
+svn export --force ${dai_daimonin_repo}/trunk/project_tools/bash-scripts/dai_setvars.sh ${dai_home}/${dai_setvars_sh}
+svn export --force ${dai_daimonin_repo}/trunk/project_tools/bash-scripts/dai_refresh.sh ${dai_home}/${dai_refresh_sh}
+svn export --force ${dai_daimonin_repo}/trunk/project_tools/bash-scripts/dai_reboot.sh ${dai_home}/${dai_reboot_sh}
+svn export --force ${dai_daimonin_repo}/trunk/project_tools/perl-scripts/tileset_updater.pl ${dai_home}/tileset_updater.pl
+
+echo
+echo "##################################"
+echo "### Cleanup SVN working copies ###"
+echo "##################################"
+
+if [ -d ${dai_svndir} ]
+then
+    svn cleanup ${dai_svndir}/daimonin/${dai_gameserver}/arch
+    svn cleanup ${dai_svndir}/daimonin/${dai_gameserver}/maps
+    svn cleanup ${dai_svndir}/daimonin/${dai_gameserver}/server
+    svn cleanup ${dai_svndir}/gridarta
+
+    if [ -e ${dai_home}/stream ]
+    then
+        read stream < ${dai_home}/stream
+
+        if [ ${stream} != "none" ]
+        then
+            rm -rf ${dai_svndir}/daimonin/stream/*
+        fi
+    fi
+
+    if [ -n ${dai_use_newarch} ]
+    then
+        svn cleanup ${dai_svndir}/daimonin/newarch/arch
+        svn cleanup ${dai_svndir}/daimonin/newarch/maps
+    fi
+
+    if [ -n ${dai_daiserv_repo} ]
+    then
+        svn cleanup ${dai_svndir}/daiserv/arch
+        svn cleanup ${dai_svndir}/daiserv/maps
+    fi
+
+    if [ -n ${dai_mapserv_repo} ]
+    then
+        svn cleanup ${dai_svndir}/mapserv/arch
+        svn cleanup ${dai_svndir}/mapserv/maps
+    fi
+fi
---
+++ trunk/project_tools/bash-scripts/dai_setvars.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Set and export variables needed by other scripts.
+
+########
+# Local directories
+
+# The home directory under which everything else takes place. You need rw
+# access here.
+export dai_home="${HOME}"
+
+# Checkouts of the various repos go here.
+export dai_svndir="${dai_home}/svn"
+
+# The server, arches, maps, and Gridarta go here.
+export dai_builddir="${dai_home}/build"
+
+# If not null, server logs go here. If null, they go in
+# ${builddir}/server/data/log.
+export dai_logsdir=""
+
+
+########
+# File/process names
+
+# Probably you have no reason to change these names, but if you do, you can.
+export dai_setvars_sh="dai_setvars.sh"
+export dai_refresh_sh="dai_refresh.sh"
+export dai_reboot_sh="dai_reboot.sh"
+export dai_time_data="dai_time.data"
+export dai_server="daimonin_server"
+
+
+########
+# SVN repository URLs
+
+# These are public reposiitories so anyone can access them (and the scripts
+# won't work without them).
+export dai_daimonin_repo="https://daimonin.svn.sourceforge.net/svnroot/daimonin"
+export dai_gridarta_repo="https://gridarta.svn.sourceforge.net/svnroot/gridarta"
+
+# If not null, these point to repositories containg maps and arches which are
+# added to the public ones. If null, they are ignored.
+export dai_daiserv_repo=""
+export dai_mapserv_repo=""
+
+
+########
+# Miscellaneous
+
+# The precise part of the Daimonin SVN you want to checkout and build. This
+# determines the basic type of gameserver to build. It should be one of
+# 'trunk', 'main', or 'test' (for historical reasons, the latest development
+# area is called trunk). You probably won't need to change this often, if at
+# all. If in doubt, leave it as this default.
+export dai_gameserver="trunk"
+
+# The precise part of the Gridarta SVN you want to checkout and build. You
+# probably won't need to change this often. If in doubt, leave it as this
+# default.
+export dai_gridarta_ttbs="tags/0.8.1"
+
+# If this is not null, the arches and maps from
+# ${dai_daimonin_repo}/streams/newarch will also be added. If null, they won't
+# be.
+export dai_use_newarch=""
+
+# Any CFLAGS you want to pass to make. Foe example, use -DDAI_DEVELOPMENT_CODE
+# and/or -DDAI_DEVELOPMENT_CONTENT to compile a development server (note that
+# if ${dai_gameserver} == "trunk" both these flags will already be defined by
+# default).
+export dai_cflags=""
+
+# If not null, the server will be run through valgrind, with the valgrind output
+# being logged in the server techlog. If null, valgrind will not be used.
+export dai_use_valgrind=""
+
+# Any options you want to pass to the server. Do not include -log as this must
+# be handled via the reboot script.
+export dai_server_options=""
