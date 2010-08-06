41f81fda57e5ee147863191d0fa5f768602063ed
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2010-08-06 13:34:30 +0000

    Bump DAI_VERSION to 0.10.1.

--- client/src/include/protocol.h
+++ client/src/include/protocol.h
@@ -26,7 +26,7 @@
 
 #define DAI_VERSION_RELEASE 0
 #define DAI_VERSION_MAJOR 10
-#define DAI_VERSION_MINOR 0
+#define DAI_VERSION_MINOR 1
 #define PROTOCOL_VERSION 991031
 
 /* min & max length of player & account names & password
