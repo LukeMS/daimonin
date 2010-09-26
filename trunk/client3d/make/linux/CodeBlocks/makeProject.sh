7ac8f63f53986f9a7984007005382a4927f11c78
polyveg <polyveg@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2010-09-26 19:34:52 +0000

    cleanups

--- trunk/client3d/make/linux/CodeBlocks/makeProject.sh
+++ trunk/client3d/make/linux/CodeBlocks/makeProject.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd ../../../
-#cmake -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - Unix Makefiles" .
-cmake -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - Unix Makefiles" .
+#cmake  -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug .
+cmake  -G "CodeBlocks - Unix Makefiles" -DCMAKE_BUILD_TYPE=Release .
