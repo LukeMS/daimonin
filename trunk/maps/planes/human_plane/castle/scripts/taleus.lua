3344c68f7aa84410931e81ba3d054b1a608b64e6
franklinb <franklinb@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2011-07-16 17:25:08 +0000

    Copy maps/scripts from mapwiz to trunk.

--- trunk/maps/planes/human_plane/castle/scripts/taleus.lua
+++ trunk/maps/planes/human_plane/castle/scripts/taleus.lua
@@ -1,46 +1,52 @@
 -- merc_taleus.lua
 -- Teaches a single archery skill. Quest missing.
 --
-require("topic_list")
-require("quest_manager")
-require("interface_builder")
 
+require("topic_list")
+require("interface_builder")
+require("quest_builder")
+
+local qb        = QuestBuilder()
 local pl        = event.activator
 local me        = event.me
-
-local q_mgr_1 = QuestManager(pl, "Rats in Stonehaven?")
-local q_status_1 = q_mgr_1:GetStatus()
-
-local ib = InterfaceBuilder()
-ib:SetHeader(me, me.name)
+local ib        = InterfaceBuilder()
+if not module_guildsLOADED then
+    require("modules/guilds")
+end
+local gstat = module_guildsGetStatus("Mercenery", pl)
 
 local skill_bow = game:GetSkillNr("bow archery")
 local skill_cbow = game:GetSkillNr("crossbow archery")
 local skill_sling = game:GetSkillNr("sling archery")
+--
 
--- Guild checks
-local guild_tag = "Mercenary"
-local guild_stat = game.GUILD_NO
-local guild_force = pl:GetGuild(guild_tag)
-if guild_force ~= nil then
-    guild_stat = guild_force.sub_type_1
+local function questGoal(questnr)
+    qb:AddQuestTarget(questnr, 1, 1, "goblin", "Hagvash"):
+    AddQuestItem(1, "quest_object", "power_crystal.101", "Raiding Party Clue")
+    pl:Sound(0, 0, 2, 0)
+    pl:Write("You take the quest '".. qb:GetName(questnr) .."'.", game.COLOR_NAVY)
 end
 
+qb:AddQuest("Rats in Stonehaven?", game.QUEST_KILLITEM, nil, nil, nil, nil, 1, questGoal, nil)
+local questnr = qb:Build(pl)
+local QStatus = qb:GetStatus(1)
+
 local function quest_reminder()
-    ib:SetTitle(q_mgr_1.name)
+    ib:SetTitle(qb:GetName(questnr))
     ib:AddMsg("Haven't you taken care of it yet? Come back when you have!\n")
-    ib:AddQuestChecklist(q_mgr_1)
+    ib:AddQuestChecklist(qb, questnr)
     ib:SetButton("Back", "hi") 
 end
     
 local function quest_icons1()
-    ib:AddSelect("Learn Archery skill of Sling ", "sling_small.101", " ")
-    ib:AddSelect("Learn Archery skill of Bow ", "bow_short.101", " ")
-    ib:AddSelect("Learn Archery skill of Crossbow ", "crossbow_small.101", " ")
+	ib:AddSelect("Learn Archery skill of Sling ", "quest complete 1", "sling_small.101", "Fastest, but hits lowest of the three")
+	ib:AddSelect("Learn Archery skill of Bow ", "quest complete 2", "bow_short.101", "Balanced between fast and strong")
+	ib:AddSelect("Learn Archery skill of Crossbow ", "quest complete 3", "crossbow_small.101", "Strongest, but slowest of the three")
 end
 
 local function quest_body1()
-    ib:SetTitle(q_mgr_1.name)
+    ib:SetHeader("st_003", me)
+    ib:SetTitle(qb:GetName(questnr))
     ib:SetMsg("We have had reports about strange noises from around one of the buildings on the east side of the town.\n\n")
     ib:SetMsg("The castle guards think it is rats and won't look into it. I must remain here in case my shipment arrives, and most other warriors are otherwise occupied. It would be great if you could investigate and take care of any problems.\n\n")
     ib:SetMsg("It seems most of the noises come from the first building on the eastern road. The woman who lives there will tell you more. Just follow this road south to the intersection by the main gates. Then go east and follow the road to the first house on the right hand side.")
@@ -49,32 +55,37 @@ local function quest_body1()
 end
 
 function topicDefault()
-    if pl:FindSkill(skill_bow) ~= nil or pl:FindSkill(skill_cbow) ~= nil or pl:FindSkill(skill_sling) ~= nil or q_status_1 == game.QSTAT_DONE then
-        ib:SetTitle("Hello again")
-        ib:AddMsg("Your basic mercenery training is probably almost finished by now. The priests in Stonhaven Village will help you to learn the final skills that you need.\n\nThe road to the village has been destroyed, unfortunately. However, I bet that the priest here in Stonehaven can help you getting there.")
-    elseif q_status_1 == game.QSTAT_NO then
-        ib:SetTitle("Greetings")
-        ib:AddMsg("\n\nHello, mercenary. I am Archery Commander Taleus.")
-        ib:AddMsg("\n\nI have taken over from my mentor, ^Chereth^, since she lost her eyes.")
-        ib:AddMsg("\n\nWell, I know a lot about ^archery^ and I ^train^ members of the mercenary guild.")
-    elseif q_status_1 == game.QSTAT_SOLVED then
-        topQuestComplete()
-        return
-    elseif q_status_1 == game.QSTAT_ACTIVE then
-        quest_reminder()
+    ib:SetHeader("st_001", me)
+    if gstat == game.GUILD_IN or module_guildsPlayerGuildless(pl) then
+        if pl:FindSkill(skill_bow) ~= nil or pl:FindSkill(skill_cbow) ~= nil or pl:FindSkill(skill_sling) ~= nil or QStatus == game.QSTAT_DONE then
+            ib:SetTitle("Hello again")
+            ib:AddMsg("Your basic mercenery training is probably almost finished by now.")
+        elseif QStatus == game.QSTAT_NO then
+            ib:SetTitle("Greetings")
+            ib:AddMsg("\n\nHello, mercenary. I am Archery Commander Taleus.")
+            ib:AddMsg("\n\nI have taken over from my mentor, ^Chereth^, since she lost her eyes.")
+            ib:AddMsg("\n\nWell, I know a lot about ^archery^ and I ^train^ members of the mercenary guild.")
+        elseif QStatus == game.QSTAT_SOLVED then
+            topicQuestComplete()
+            return
+        elseif QStatus == game.QSTAT_ACTIVE then
+            quest_reminder()
+        else
+            pl:Write(me.name .." has nothing to say.", game.COLOR_NAVY)
+            return
+        end
     else
-        pl:Write(me.name .." has nothing to say.", game.COLOR_NAVY)
-        pl:Interface(game.GUI_NPC_MODE_NO) 
-        return
+        ib:SetTitle("Hello")
+        ib:SetMsg("I'm sorry, but I cannot teach you an archery skill due to your guild.")
+        ib:AddMsg(" You can either join the Mercenery guild or leave your current guild.")
     end
-    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
 
 -- Player asks for quests
 function topQuest()
-    if q_status_1 == game.QSTAT_ACTIVE then
+    if QStatus == game.QSTAT_ACTIVE then
         quest_reminder()
-    elseif q_status_1 == game.QSTAT_NO then 
+    elseif QStatus == game.QSTAT_NO then 
         quest_body1()
         ib:SelectOff()
         ib:SetAccept(nil, "accept quest") 
@@ -83,81 +94,28 @@ function topQuest()
         topicDefault()
         return
     end
-
-    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
-
 -- accepted: start the quest
 function topAccept()
-    if q_status_1 == game.QSTAT_NO then
+    if QStatus == game.QSTAT_NO then
         quest_body1()
         ib:SelectOff()
-        if q_mgr_1:RegisterQuest(game.QUEST_ITEM, ib) then
-            q_mgr_1:AddQuestItem(1, "quest_object", "power_crystal.101", "Raiding Party Clue")
-            pl:Sound(0, 0, 2, 0)
-            pl:Write("You take the quest '".. q_mgr_1.name .."'.", game.COLOR_NAVY)
-        end
-    else
-        topicDefault()
+        qb:RegisterQuest(questnr, me, ib)
+   else
+       topicDefault()
         return
     end
-    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
 
--- try to finish: check the quest
-function topQuestComplete()
-    ib:SetTitle(q_mgr_1.name)
-    if q_status_1 == game.QSTAT_ACTIVE then
-        quest_reminder()
-    else
-        ib:AddMsg("Very well done! It was worse than I thought. I wonder where those kobolds came from--I thought we'd driven most of them out.\n\n")
-        ib:AddMsg("I will take the crystal you found to Jahrlen, he might be able to undestand it.\n\n")
-        ib:AddMsg("Now you must choose your archery skill:")
-        quest_icons1()
-        ib:AddQuestChecklist(q_mgr_1)
-        ib:SetAccept(nil, "reward") 
-        ib:SetDecline(nil, "hi") 
-	end
-	pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
-end
-
--- done: finish quest and give reward
-function topQuestReward(reward)
-    local rewards = {
-        ["#1"] = { skill = skill_sling, weapon = "sling_small", ammo = "sstone" },
-        ["#2"] = { skill = skill_bow, weapon = "bow_short", ammo = "arrow" },
-        ["#3"] = { skill = skill_cbow, weapon = "crossbow_small", ammo = "bolt" }
-    }
-    
-    if q_status_1 ~= game.QSTAT_SOLVED or not rewards[reward] then
-        topicDefault()
-        return
-    end
-    
-    pl:AcquireSkill(rewards[reward].skill, game.LEARN)
-    pl:CreateObjectInsideEx(rewards[reward].weapon, game.IDENTIFIED,1)
-    pl:CreateObjectInsideEx(rewards[reward].ammo, game.IDENTIFIED,20)
-
-    q_mgr_1:RemoveQuestItems()
-    q_mgr_1:Finish()
-    pl:Sound(0, 0, 2, 0)
-    ib:SetMsg("Very well done! Here is your reward!")
-    ib:SetButton("Ok", "hi") 
-    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
-end
-
---
--- Rumors
---
-
-local function topChereth()
+function topChereth()
+    ib:SetHeader("st_004", me)
     ib:SetTitle("Chereth")
     ib:SetMsg("\n\nYes, Chereth lost her eyes in a major battle. When you find her, she can tell you more.")
     ib:SetButton("Back", "hi")
-    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
 
-local function topArchery()
+function topArchery()
+    ib:SetHeader("st_003", me)
     ib:SetTitle("Archery Info")
     ib:SetMsg("\n\nYes, there are three archery skills:\n")
     ib:AddMsg("\nBow Archery is the most common, firing arrows.\n")
@@ -173,22 +131,57 @@ local function topArchery()
         ib:AddLink("Could you tell me what this 'quest' is?", "explain quest")
     end
     ib:SetButton("Back", "hi")
-    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+end
+function topicQuestComplete(reward)
+    if reward == nil then
+		if QStatus == game.QSTAT_ACTIVE then
+			quest_reminder()
+		else	
+			ib:AddMsg("Very well done! It was worse than I thought. I wonder where those kobolds came from--I thought we'd driven most of them out.\n\n")
+			ib:AddMsg("I will take the crystal you found to Jahrlen, he might be able to understand it.\n\n")
+			ib:AddMsg("Now you must choose your archery skill:")
+			quest_icons1()
+		end
+	else
+		if reward == "1" then
+			ib:SetTitle("Sling archery")
+			ib:SetMsg("Ah, you chose sling archery. An excellent choice!")
+			pl:AcquireSkill(skill_sling, game.LEARN)
+			pl:CreateObjectInsideEx("sling_small", game.IDENTIFIED,1)
+			pl:CreateObjectInsideEx("sstone", game.IDENTIFIED,20)
+		elseif reward == "2" then
+			ib:SetTitle("Bow archery")
+			ib:SetMsg("Ah, you chose bow archery. An excellent choice!")
+			pl:AcquireSkill(skill_bow, game.LEARN)
+			pl:CreateObjectInsideEx("bow_short", game.IDENTIFIED,1)
+			pl:CreateObjectInsideEx("arrow", game.IDENTIFIED,20)
+		elseif reward == "3" then
+			ib:SetTitle("Crossbow archery")
+			ib:SetMsg("Ah, you chose crossbow archery. An excellent choice!")
+			pl:AcquireSkill(skill_cbow, game.LEARN)
+			pl:CreateObjectInsideEx("crossbow_small", game.IDENTIFIED,1)
+			pl:CreateObjectInsideEx("bolt", game.IDENTIFIED,20)
+		end
+		qb:Finish(1)
+	end
 end
 
 tl = TopicList()
 tl:AddGreeting(nil, topicDefault)
 tl:SetDefault(topicDefault)
-if q_mgr_1:GetStatus() < game.QSTAT_DONE then
-    -- Can only take the quest if guildmember
-    if guild_stat == game.GUILD_IN then
-        tl:AddTopics({"quest", "explain%s+quest"}, topQuest)
-        tl:AddTopics({"accept", "accept%s+quest"}, topAccept) 
+if gstat == game.GUILD_IN or module_guildsPlayerGuildless(pl) then
+    if qb:GetStatus(1) < game.QSTAT_DONE then
+        -- Can only take the quest if guildmember
+        if guild_stat == game.GUILD_IN then
+            tl:AddTopics({"quest", "explain%s+quest"}, topQuest)
+            tl:AddTopics({"accept", "accept%s+quest"}, topAccept) 
+        end
+        if qb:GetStatus(1) == game.QSTAT_SOLVED then
+            tl:AddTopics({"complete", "quest%s+complete"}, topicQuestComplete) 
+            tl:AddTopics({"complete", "quest%s+complete%s*#?(%d*)"}, topicQuestComplete)
+        end
     end
-    tl:AddTopics({"complete", "quest%s+complete"}, topQuestComplete) 
-    tl:AddTopics({"reward%s+(#%d+)"}, topQuestReward)
 end
 tl:AddTopics({"archery", "train"}, topArchery)
-tl:AddTopics("chereth", topChereth)
-
-tl:CheckMessage(event)
+tl:AddTopics({"chereth", "rumors", "rumours", "rumor", "rumour"}, topChereth)
+ib:ShowSENTInce(game.GUI_NPC_MODE_NPC, tl:CheckMessage(event, true))
