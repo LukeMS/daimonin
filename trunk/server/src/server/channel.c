abcdfe83a55e46debe7052af1539c7c253f2817f
franklinb <franklinb@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2011-12-07 01:46:57 +0000

    Fix a typo which makes the new channel system unable to operate. The first parameter, the socket, of the SOCKBUF_ADD_TO_SOCKET macro that was called had the wrong socket listed.

--- trunk/server/src/server/channel.c
+++ trunk/server/src/server/channel.c
@@ -713,7 +713,7 @@ void sendChannelMessage(player *pl,struct player_channel *pl_channel, char *para
 
     for (cpl=pl_channel->channel->players;cpl;cpl=cpl->next_player)
         if (cpl->pl->channels_on)
-            SOCKBUF_ADD_TO_SOCKET(ns, sockbuf);
+            SOCKBUF_ADD_TO_SOCKET(&cpl->pl->socket, sockbuf);
 
     SOCKBUF_COMPOSE_FREE(sockbuf);
     return;
