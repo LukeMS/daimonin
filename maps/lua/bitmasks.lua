1fb8461964a5b79acda98c3bcfc0dc618365197c
franklinb <franklinb@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2012-08-04 23:55:14 +0000

    Rewrite my Lua bitmask implementation to be a more OOP-ish approach. Makes it cleaner to use.

--- maps/lua/bitmasks.lua
+++ maps/lua/bitmasks.lua
@@ -1,73 +1,206 @@
-------------------------------
+------------------
+--
 -- bitmasks.lua
-------------------------------
--- Adds Lua support for bitmasks, up to 8-bit bytes are supported.
-------------------------------
+--
+------------------
+-- A useful tool for using bitflags supplied by the server in Lua scripts. An
+-- example of this (which is used throughout this file) is GMaster modes. The
+-- server uses bitflags for these but Lua doesn't like that by default. This
+-- library allows Lua scripts to easily check gmaster modes without a bunch
+-- of nasty if-or-then statements.
+------------------
 
-function _byte()
-    return {0, 0, 0, 0, 0, 0, 0, 0,}
-end
-powers = {1, 2, 4, 8, 16, 32, 64, 128,}
+Byte = { }
 
-function num_to_byte(num)
--- A nifty rule about powers of two:
--- The power of two directly before the represented value is always a bit in that number.
--- For example, in 55, 32 is the first power below it. So 32 is a bit of 55.
--- So, this function will loop backwards until it finds a power below num, and raises that bit to 1.
--- Then it subtracts that power from num, and loops.
+local default_bitsize = 8
 
-    local byte = _byte()
+-- Create a new byte and set each bit to 0.
+local function new_byte(bitsize)
+
+    local byte = { }
     
-    if num == 0 then return byte end
+    for i = 1, bitsize do
     
-    --if num == 0 then return byte end
-    for i = table.getn(powers), 1, -1 do
-        -- First, make sure num isn't already a power of two. This can occur if either:
-        -- The passed arg is already a power of two; or
-        -- powers[i] is equal to the smallest bit of num.
-        if powers[i] == num then
-            byte[i] = 1
-            break
-        end
-        if powers[i] < num then
-            byte[i] = 1
-            num = num - powers[i]
-        end
+        -- Fill our byte with 0's.
+        table.insert(byte, 0)
+    
     end
     
-    
     return byte
+
 end
 
-function byte_to_num(byte)
+-- Returns the base-two logarithm of the numerical arg.
+local function log2(num)
+
+    return math.log(num) / math.log(2)
+
+end
+
+-- Create a new byte with an optional bitsize (defaults to defualt_bitsize).
+function Byte:New(bitsize)
+
+    local b = {
+                byte = {},
+                size  = default_bitsize
+              }
+    
+    if bitsize then
+    
+        b.size = bitsize
+    
+    end
+    
+    b.byte = new_byte(b.size)
+
+    setmetatable(b, { __metatable = Byte,
+                      __index = Byte })
+
+    return b
+    
+end
+
+-- Creates the constructor for the Byte class.
+setmetatable(Byte, { __call = Byte.New })
+
+------------------
+-- Purpose: Creates a decimal representation of the byte table.
+-- Args:    none
+-- Returns: (number) The numerical representation of the byte.
+-- Example: local req_modes = mode:ToNumber()
+------------------
+function Byte:ToNumber()
+
     local num = 0
     
-    for i = 0, table.getn(_byte()) do
-        if byte[i] == 1 then
+    for i = 0, table.getn(self.byte) do
+    
+        if self.byte[i] == 1 then
+        
             num = num + math.pow(2, i - 1)
+            
         end
+        
     end
     
     return num
+
 end
 
-function mask(num, ...)
-    -- Return a bitmask created by masking num with the rest of the args.
-    if arg.n == 0 then return num end
+------------------
+-- Purpose: Puts the binary representation of a number into the byte table.
+-- Args:    A decimal number to be converted to binary.
+-- Returns: nil
+-- Example: mode:FromNumber(pl:GetGmasterMode())
+-- Notes:   By Lua's design, the byte table expands to fit the number.
+------------------
+function Byte:FromNumber(num)
 
-    -- First, convert num to a bitwise format.
-    local byte = num_to_byte(num)
+    assert(type(num) == 'number', 'Arg #1 must be a number!')
+
+    -- No signed bytes!
+    num = math.abs(num)
+    
+    local bit = 0
+
+    while (num > 0) do
+    
+        bit = math.floor(log2(num))
+        self.byte[bit + 1] = 1
+        num = num - math.pow(2, bit)
+    
+    end
+    
+    return nil
+
+end
+
+------------------
+-- Purpose: Flags its args onto the byte table. This is similar to the &=
+--              operator in C/C++.
+-- Args:    One or more powers of two (bitflags) such as game.GMASTER_MODE_*
+--              constants
+-- Returns: nil
+-- Example: mode:Mask(game.GMASTER_MODE_VOL, game.GMASTER_MODE_GM)
+------------------
+function Byte:Mask(...)
+
+    if arg.n == 0 then
+    
+        return self
+    
+    end
+    
+    for a = 1, arg.n do
+
+        assert(type(arg[a]) == 'number', 'Arg #' .. a .. ' must be a number!')
+        
+        -- If num is a power of two then log2(num) should have no decimal.
+        assert(math.floor(log2(arg[a])) == log2(arg[a]), 'Arg #' .. a .. ' must be a power of 2!')
+        
+        -- No signed bytes!
+        arg[a] = math.abs(arg[a])
+    
+    end
     
     -- Loop through all passed masks.
     for a = 1, arg.n do
-        local ind = -1
-        for i = 0, table.getn(powers) do
-            if powers[i] == arg[a] then
-                byte[i] = 1
-                break
-            end
-        end
+    
+        self.byte[log2(arg[a]) + 1] = 1
+        
     end
 
-    return byte_to_num(byte)
+    return self
+    
+end
+
+------------------
+-- Purpose: Checks if a specified bitflag is marked in this byte. Similar to
+--              the & operator in C/C++.
+-- Args:    num - A power of two that is being searched for in the byte table.
+-- Returns: (boolean) Whether or not the specified flag is marked in the byte.
+-- Example: if mode:Check(game.GMASTER_MODE_VOL) then ... end
+------------------
+function Byte:Check(num)
+
+    assert(type(num) == 'number', 'Arg #1 must be a number!')
+    
+    -- If num is a power of two then log2(num) should have no decimal.
+    assert(math.floor(log2(num)) == log2(num), 'Arg #1 must be a power of 2!')
+    
+    -- No signed bytes!
+    num = math.abs(num)
+
+    if self.byte[log2(num) + 1] == 1 then
+    
+        return true
+        
+    end
+    
+    return false
+
+end
+
+------------------
+-- Purpose: Adds each bit in the byte table and its index in that table
+--              to a string delimited by spaces and newlines.
+-- Args:    none
+-- Returns: (string) Representation of the byte table.
+-- Example: mode:Debug()
+--              >> 1   0
+--              >> 2   1
+--              >> 3   0
+--              >> 4   1
+------------------
+function Byte:Debug()
+
+    local str = ''
+    
+    for i,v in ipairs(self.byte) do
+    
+        str = str .. i .. '   ' .. v .. '\n'
+        
+    end
+
+    return str
 end
