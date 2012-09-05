9e96416f5dc382bacab864035c8d50129828342f
jsa-smith <jsa-smith@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2012-09-05 15:19:21 +0000

    Revert previous commit made by fixMimeAndIgnore.sh

--- trunk/bin/win32/w32_start_editor.bat
+++ trunk/bin/win32/w32_start_editor.bat
@@ -3,5 +3,5 @@ cd ..\..\editor
 IF EXIST DaimoninEditor.jar.tmp (
 rename DaimoninEditor.jar DaimoninEditor.jar.bak
 rename DaimoninEditor.jar.tmp DaimoninEditor.jar)
-javaw.exe -Xmx1024m -Xms1024m -jar DaimoninEditor.jar 
+javaw.exe -jar DaimoninEditor.jar
 cd ..\bin\win32
