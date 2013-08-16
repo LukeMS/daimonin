37d3f10f18d4f365626205bac8f8c2da10c6642a
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2013-08-16 14:26:34 +0000

    Update scripts from daiserv to not uuse quest_check.lua which is hideously outdated (these are still outdated, but not as much).

--- maps/planes/human_plane/castle/scripts/felbe.lua
+++ maps/planes/human_plane/castle/scripts/felbe.lua
@@ -1,133 +1,96 @@
--- template for a "item quest" script
 require("topic_list")
-require("quest_check")
 require("interface_builder")
 
-local pl        = event.activator
-local me        = event.me
-local msg       = string.lower(event.message)
-
-local q_name_1  = "Dev Item Test Quest"
-local q_step_1  = 0
-local q_level_1  = 1
-local q_skill_1  = game.ITEM_SKILL_NO
-
-local q_obj_1   = pl:GetQuest(q_name_1)
-local q_stat_1  = Q_Status(pl, q_obj_1, q_step_1, q_level_1, q_skill_1)
+local pl = event.activator
+local me = event.me
+local ds = DataStore("felbe_letter_quest", pl)
 
 local ib = InterfaceBuilder()
 ib:SetHeader(me, me.name)
 
+local ds_msg = ds:Get("felbe_msg")
+
 local function topicDefault()
-if q_stat_1 < game.QSTAT_DONE then
-ib:AddMsg("[DEVMSG] The quest status is: ".. q_stat_1 .."\n\n")
-ib:AddIcon(0,"booze_generic.101",0)
-if q_stat_1 == game.QSTAT_NO then
-ib:SetTitle("Item Test Quest")
-ib:AddMsg("[INTRO] A quest which needs an item delivered. The item is inside the quest object inside the player which is given the player when this quest is accepted. There is a QUEST_TRIGGER inside a object (chest or mob) which will move a visible copy to the players inventory.")
-ib:AddLink("Start Item Test Quest", "startq1")
-else
-ib:SetTitle("Item Test Quest solved?")
-ib:AddMsg("[pending] You has done the quest?")
-ib:AddLink("Finish Item Test Quest", "checkq1")
-end
-else
-pl:Write(me.name .." has nothing to say.", game.COLOR_NAVY)
-pl:Interface(game.GUI_NPC_MODE_NO) 
-return
-end
-pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
-end
-
--- quest body (added to player quest obj for quest list)
-local function quest_icons1()
-ib:AddIcon("quest-reward-name", "shield.101", "i am the stats/description line") 
-end
-local function quest_body1()
-ib:SetMsg("[WHY] Get the item this test quest needs.")
-ib:SetDesc("[WHAT] Bring me a °Item Test Helm°.\nKill the ant or open the chest.", 1, 2, 0, 0)
-end
-
--- start: accept or decline the quest
-local function topStartQ1()
-if q_stat_1 ~= game.QSTAT_NO then
-topicDefault()
-else
-ib:SetTitle(q_name_1)
-quest_body1()
-quest_icons1()
-ib:SetAccept(nil, "acceptq1") 
-ib:SetDecline(nil, "hi") 
-pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
-end
+    if pl:CheckInventory(1, nil, "Felbe's Letter", nil, -1) == nil and pl:CheckInventory(1, nil, "Guard Captain Aldus's Letter", nil, -1) == nil then
+        if ds_msg == "felbe_letter_done" then
+            me:SayTo(pl, "Talk to Aldus for your reward!")
+            pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+            return
+        elseif ds_msg == "felbe_letter_really_done" then
+            pl:Write(me.name.." thanks you for helping out.", 2)
+            pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+            return
+        else
+            ib:SetTitle("Hello There") -- set title to whatever you want
+            ib:SetMsg("Hello, the name's Felbe.\nThe forests seem quiet these days, but don't let that fool you. The forest is always quiet, but if you pay close attention you will begin to uncover its mysteries.") -- set a first message
+            ib:AddMsg("\n\nEither the forest is playing a trick on me, or someone has been tampering with my letters. In any case, I need help getting a message safely to the castle.\nWill you help?") -- add another message to the first \n is a space in interface
+            ib:SetAccept("I'll Help", "accept") -- change nil to something like "Sure" - be sure you set it with quotes!
+            ib:SetDecline("Can't...", "quit") -- same as above but this is the bye button
+        end
+    elseif pl:CheckInventory(1, nil, "Guard Captain Aldus's Letter", nil, -1) then
+        ib:SetTitle(me.name.." thanks to you.")
+        local aldus_letter = pl:CheckInventory(1, nil, "Guard Captain Aldus's Letter", nil, -1)
+        aldus_letter:Remove()
+        ib:AddMsg("\nHe sent you back? Someone must be intercepting our letters then. I'll be on my guard, and I'd advise you to do the same. May your arrows fly for the heart and may you have a safe journey.") -- msg after you brang to Felbe the Aldus's Letter
+        ds:Set("felbe_msg", "felbe_letter_done")
+    elseif pl:CheckInventory(1, nil, "Felbe's Letter", nil, -1) then
+        me:SayTo(pl, "You've already got the letter -- now go and bring it to Aldus!")
+        pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+        return
+    else
+        pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+        return
+    end
+    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
 
--- accepted: start the quest
-local function topAcceptQ1()
-if q_stat_1 == game.QSTAT_NO then
-quest_body1()
-quest_icons1()
-q_obj_1 = pl:AddQuest(q_name_1, game.QUEST_ITEM, q_step_1, q_step_1, q_level_1, q_skill_1, ib:Build())
-if q_obj_1 ~= null then
-q_obj_1:AddQuestItem(1, "quest_object", "helm_leather.101", "Item Test Helm")
-q_stat_1 = Q_Status(pl, q_obj_1, q_step_1, q_level_1, q_skill_1)
-pl:Sound(0, 0, 2, 0)
-pl:Write("You take the quest '"..q_name_1.."'.", game.COLOR_NAVY)
-end
-ib = InterfaceBuilder()
-ib:SetHeader(me, me.name)
-end
-topicDefault()
-end
-
--- try to finish: check the quest
-local function topCheckQ1()
-if q_stat_1 == game.QSTAT_NO then
-topicDefault()
-else
-ib:SetTitle("FINAL CHECK: Item Test Quest")
-ib:SetMsg("[DEVMSG] The quest status is: ".. q_stat_1 .."\n\n")
-if q_stat_1 ~= game.QSTAT_SOLVED then
-ib:AddMsg("[not-done-text] Come back if you have it!\n")
-Q_List(q_obj_1, ib)
-ib:SetButton("Back", "hi") 
-else
-ib:AddMsg("[final-text] Very well done! You found the helm.\n")
-ib:SetDesc("here it is...", 1, 2, 0, 0)
-quest_icons1()
-Q_List(q_obj_1, ib)
-ib:SetAccept(nil, "finishq1") 
-ib:SetDecline(nil, "hi") 
-end
-pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
-end
+local function topicAccept()
+    if pl:CheckInventory(1, nil, "Felbe's Letter", nil, -1) == nil or pl:CheckInventory(1, nil, "Guard Captain Aldus's Letter", nil, -1) == nil then
+        ib:SetTitle("Instructions") -- set title to whatever you want again
+        ib:SetMsg("Great. Take the letter to Stonehaven Castle and get it to the ^captain of the guard^, Aldus. We are supposed to be in contact in case I find something odd here or he needs my services elsewhere. \nI haven't heard from him in at least a week and I suspect we have someone intercepting our messages.") -- set a first message again
+        ib:AddMsg("\n\nBe careful on your way. They've been intercepting our ^carriers^ and I suspect they might try to intercept even you. Aldus will handle it once he knows what's going on.") -- add another message ( remove if you don't need another message )
+        if pl:CheckInventory(1, nil, "Felbe's Letter", nil, -1) then
+            ib:AddMsg("\n\nYou already have the letter, so you best be heading northeast towards the castle.") -- msg that shows if you already have the Felbe's Letter
+        elseif pl:CheckInventory(1, nil, "Felbe's Letter", nil, -1) == nil then
+            local letter = pl:CreateObjectInside("letter", 1,1)
+            letter.message = "" -- Set the message for letter
+            letter.name = "Felbe's Letter"
+            letter.f_identified = 1
+            letter.f_startequip = 1
+            ds:Set("felbe_msg", "felbe_letter")
+            ib:AddMsg("\n\nHere's the letter, now hurry and go.")
+        end
+    else
+        pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+        return
+    end
+    ib:SetButton("Back", "hi")
+    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
 
--- done: finish quest and give reward
-local function topFinishQ1()
-if q_stat_1 ~= game.QSTAT_SOLVED then
-topicDefault()
-else
-q_obj_1:RemoveQuestItem()
-q_obj_1:SetQuestStatus(-1)
-q_stat_1 = game.QSTAT_DONE
-pl:Sound(0, 0, 2, 0)
-pl:CreateObjectInsideEx("shield", 1,1)
-pl:AddMoneyEx(1,2,0,0)
-ib:SetTitle("QUEST END: Item Test Quest")
-ib:SetMsg("Very well done! Here is your reward!")
-ib:SetButton("Ok", "hi") 
-pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+local function topicCarriers()
+    ib:SetTitle("Carriers")
+    ib:SetMsg("My carriers? Oh, I use hawks, generally. Sometimes I use Theodore, my dog, because he's much more reliable in the weather.")
+    ib:SetButton("Back", "hi")
+    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
+
+local function topicAldus()
+    ib:SetTitle("Aldus, Captain of the Guard")
+    ib:SetMsg("Aldus is an honorable man; he fits his position nicely. I would never doubt his word, even if King <insert name here> himself spoke against him.")
+    ib:SetButton("Back", "hi")
+    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+end
+
+local function topicQuit()
+    pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
 
 tl = TopicList()
 tl:AddGreeting(nil, topicDefault)
+tl:AddTopics("quit", topicQuit)
+tl:AddTopics("accept", topicAccept)
+tl:AddTopics("carriers", topicCarriers)
+tl:AddTopics("captain of the guard", topicAldus)
 tl:SetDefault(topicDefault)
-if q_stat_1 < game.QSTAT_DONE then
-tl:AddTopics("startq1", topStartQ1) 
-tl:AddTopics("acceptq1", topAcceptQ1) 
-tl:AddTopics("checkq1", topCheckQ1) 
-tl:AddTopics("finishq1", topFinishQ1) 
-end
 tl:CheckMessage(event)
--- maps/planes/human_plane/castle/scripts/lost_booze.lua
+++ maps/planes/human_plane/castle/scripts/lost_booze.lua
@@ -1,79 +1,79 @@
 -- template for a "item quest" script
 require("topic_list")
-require("quest_check")
+require("quest_builder")
 require("interface_builder")
 
 local pl        = event.activator
 local me        = event.me
 local msg       = string.lower(event.message)
+local qb        = QuestBuilder()
 
 local q_name_1  = "Lost Booze"
 local q_step_1  = 0
-local q_level_1  = 1
+local q_level_1  = 4
 local q_skill_1  = game.ITEM_SKILL_NO
 
-local q_obj_1   = pl:GetQuest(q_name_1)
-local q_stat_1  = Q_Status(pl, q_obj_1, q_step_1, q_level_1, q_skill_1)
-
 local ib = InterfaceBuilder()
-ib:SetHeader(me, me.name)
+
+local function questGoal(questnr)
+    qb:AddQuestItem(questnr, 1, "quest_object", "booze.101", "Boozie")
+    pl:Sound(0, 0, 2, 0)
+    pl:Write("You take the quest '".. qb:GetName(questnr) .."'.", game.COLOR_NAVY)
+end
+
+qb:AddQuest("Lost Booze", game.QUEST_ITEM, q_level_1, q_skill_1, nil, nil, 1, questGoal, nil)
+local questnr = qb:Build(pl)
+local q_stat_1 = qb:GetStatus(1)
 
 local function topicDefault()
-if q_stat_1 < game.QSTAT_DONE then
-if q_stat_1 == game.QSTAT_NO then
-ib:SetTitle("Lost Booze")
-ib:AddMsg("'Lo der chap. Yer look like uh nice feller, so take yeh a set an' 'ave a bet o meh boozish... Jus' uh sec... whas tis? Someun tooked meh boozie! Why dat varmet, I swears, wen I git em--ah, oh. I mustuv lef meh boozun a 'ome in meh booze ches'. Would yeh be uh nice feller an' fetch meh boozit fo' some coin?")
-ib:AddLink("Yessir", "startq1")
-else
-ib:SetTitle("Found Meh Boozie?")
-ib:AddMsg("Yeh found meh boozie yet? I been awaitin 'ere fo' it.")
-ib:AddLink("Yup, here it is", "checkq1")
-end
-else
-pl:Write(me.name .." has nothing to say.", game.COLOR_NAVY)
-pl:Interface(game.GUI_NPC_MODE_NO) 
-return
-end
-pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+	if q_stat_1 < game.QSTAT_DONE then
+		if q_stat_1 == game.QSTAT_NO then
+			ib:SetTitle("Lost Booze")
+			ib:AddMsg("'Lo der chap. Yer look like uh nice feller, so take yeh a set an' 'ave a bet o meh boozish... Jus' uh sec... whas tis? Someun tooked meh boozie! Why dat varmet, I swears, wen I git em--ah, oh. I mustuv lef meh boozun a 'ome in meh booze ches'. Would yeh be uh nice feller an' fetch meh boozit fo' some coin?")
+			ib:AddLink("Yessir", "startq1")
+		else
+			ib:SetTitle("Found Meh Boozie?")
+			ib:AddMsg("Yeh found meh boozie yet? I been awaitin 'ere fo' it.")
+			ib:AddLink("Yup, here it is", "checkq1")
+		end
+	else
+		pl:Write(me.name .." has nothing to say.", game.COLOR_NAVY)
+		pl:Interface(game.GUI_NPC_MODE_NO) 
+		return
+	end
+	pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
 end
 
 -- quest body (added to player quest obj for quest list)
 local function quest_icons1()
-ib:AddIcon("Silver", "silvercoin.101", "A Silver Coin") 
+	ib:AddIcon("Silver", "silvercoin.101", "A Silver Coin") 
 end
 local function quest_body1()
-ib:SetMsg("Bring meh boozie from meh 'ouse fer meh. Meh 'ouse is aroun' on the east part o' town.")
-ib:SetDesc("Rememer, brin' meh °Boozie° from meh booze ches'.", 0, 4, 0, 0)
+	ib:SetMsg("Bring meh boozie from meh 'ouse fer meh. Meh 'ouse is aroun' on the east part o' town.")
+	ib:SetDesc("Rememer, brin' meh ~Boozie~ from meh booze ches'.", 0, 4, 0, 0)
 end
 
 -- start: accept or decline the quest
 local function topStartQ1()
-if q_stat_1 ~= game.QSTAT_NO then
-topicDefault()
-else
-ib:SetTitle(q_name_1)
-quest_body1()
-ib:SetAccept(nil, "acceptq1") 
-ib:SetDecline(nil, "hi") 
-pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
-end
+	if q_stat_1 ~= game.QSTAT_NO then
+		topicDefault()
+	else
+		ib:SetTitle(q_name_1)
+		quest_body1()
+		ib:SetAccept(nil, "acceptq1") 
+		ib:SetDecline(nil, "hi") 
+		pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+	end
 end
 
 -- accepted: start the quest
 local function topAcceptQ1()
-if q_stat_1 == game.QSTAT_NO then
-quest_body1()
-q_obj_1 = pl:AddQuest(q_name_1, game.QUEST_ITEM, q_step_1, q_step_1, q_level_1, q_skill_1, ib:Build())
-if q_obj_1 ~= null then
-q_obj_1:AddQuestItem(1, "quest_object", "booze.101", "Boozie")
-q_stat_1 = Q_Status(pl, q_obj_1, q_step_1, q_level_1, q_skill_1)
-pl:Sound(0, 0, 2, 0)
-pl:Write("You take the quest '"..q_name_1.."'.", game.COLOR_NAVY)
-end
-ib = InterfaceBuilder()
-ib:SetHeader(me, me.name)
-end
-topicDefault()
+	if q_stat_1 == game.QSTAT_NO then
+		qb:RegisterQuest(questnr, me, ib)
+		pl:Interface(game.GUI_NPC_MODE_NO)
+	else
+		topicDefault()
+	end
 end
 
 -- try to finish: check the quest
@@ -97,19 +97,17 @@ end
 
 -- done: finish quest and give reward
 local function topFinishQ1()
-if q_stat_1 ~= game.QSTAT_SOLVED then
-topicDefault()
-else
-q_obj_1:RemoveQuestItem()
-q_obj_1:SetQuestStatus(-1)
-q_stat_1 = game.QSTAT_DONE
-pl:Sound(0, 0, 2, 0)
-pl:AddMoneyEx(0,4,0,0)
-ib:SetTitle("Lost Booze")
-ib:SetMsg("Yeh ave yerself uh day o goodun!")
-ib:SetButton("Ok", "hi") 
-pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
-end
+	if q_stat_1 ~= game.QSTAT_SOLVED then
+		topicDefault()
+	else
+		pl:Sound(0, 0, 2, 0)
+		pl:AddMoneyEx(0,4,0,0)
+		ib:SetTitle("Lost Booze")
+		ib:SetMsg("Yeh ave yerself uh day o goodun!")
+		qb:Finish(questnr)
+		ib:SetButton("Ok", "hi") 
+		pl:Interface(game.GUI_NPC_MODE_NPC, ib:Build())
+	end
 end
 
 tl = TopicList()
