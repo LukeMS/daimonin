fd030976541271b92d605f22e59433979b7f8521
smacky <smacky@a1c9b77f-4d8e-4539-8495-942979dc83e7>
2016-08-06 14:45:32 +0000

    use GETTIMEOFDAY() rather than implementation-dependent gettimeofday().
    In fact, the TPR macros will replace this code anyway.

--- server/src/tests/benchmark.h
+++ server/src/tests/benchmark.h
@@ -3,8 +3,8 @@
 
 /* Just a few definitions that we can use for benchmarking */
 
-#define timer_start(x) { struct timeval start_time, stop_time; gettimeofday(&start_time, NULL);
-#define timer_stop(nops) gettimeofday(&stop_time, NULL); print_delta_time(&start_time, &stop_time, nops); }
+#define timer_start(x) { struct timeval start_time, stop_time; GETTIMEOFDAY(&start_time);
+#define timer_stop(nops) GETTIMEOFDAY(&stop_time); print_delta_time(&start_time, &stop_time, nops); }
 
 extern void print_delta_time(struct timeval *start, struct timeval *stop, int nops);
 extern int benchmark_repetitions;
